d651ebedb689b871dbf6833091b88cac330f5f86
ishland <ishlandmc@yeah.net>
2021-02-20 08:17:04 +0800

    Null-safe cache

--- src/main/java/org/yatopiamc/c2me/mixin/threading/chunkio/MixinServerTickScheduler.java
+++ src/main/java/org/yatopiamc/c2me/mixin/threading/chunkio/MixinServerTickScheduler.java
@@ -3,7 +3,9 @@ package org.yatopiamc.c2me.mixin.threading.chunkio;
 import com.google.common.base.Preconditions;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.server.world.ServerTickScheduler;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.ChunkPos;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,14 +13,18 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.yatopiamc.c2me.common.threading.chunkio.ICachedServerTickScheduler;
 
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ForkJoinPool;
 
 @Mixin(ServerTickScheduler.class)
 public abstract class MixinServerTickScheduler implements ICachedServerTickScheduler {
 
     @Shadow public abstract ListTag toTag(ChunkPos chunkPos);
 
-    private ConcurrentHashMap<ChunkPos, ListTag> cachedNbt = new ConcurrentHashMap<>();
+    @Shadow @Final private ServerWorld world;
+    private ConcurrentHashMap<ChunkPos, Optional<ListTag>> cachedNbt = new ConcurrentHashMap<>();
 
     @Inject(method = "<init>", at = @At("RETURN"))
     private void onInit(CallbackInfo info) {
@@ -29,7 +35,7 @@ public abstract class MixinServerTickScheduler implements ICachedServerTickSched
     public void prepareCachedNbt(ChunkPos pos) {
         Preconditions.checkNotNull(pos);
         if (cachedNbt == null) cachedNbt = new ConcurrentHashMap<>();
-        cachedNbt.put(pos, toTag(pos));
+        cachedNbt.put(pos, Optional.ofNullable(toTag(pos)));
     }
 
     @Override
@@ -37,9 +43,9 @@ public abstract class MixinServerTickScheduler implements ICachedServerTickSched
         Preconditions.checkNotNull(pos);
         if (cachedNbt == null) cachedNbt = new ConcurrentHashMap<>();
         if (!cachedNbt.containsKey(pos)) {
-            new IllegalStateException("Tried to serialize ticklist with no cached nbt for chunk " + pos + "! This will affect data integrity. Incompatible mods?").printStackTrace();
-            prepareCachedNbt(pos);
+            System.err.println("Tried to serialize ticklist with no cached nbt for chunk " + pos + "! This will affect performance. Incompatible mods?");
+            return CompletableFuture.supplyAsync(() -> toTag(pos), this.world.getServer()).join();
         }
-        return this.cachedNbt.remove(pos);
+        return this.cachedNbt.remove(pos).orElse(null);
     }
 }
--- src/main/java/org/yatopiamc/c2me/mixin/threading/chunkio/MixinSimpleTickScheduler.java
+++ src/main/java/org/yatopiamc/c2me/mixin/threading/chunkio/MixinSimpleTickScheduler.java
@@ -10,6 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.yatopiamc.c2me.common.threading.chunkio.ICachedChunkTickScheduler;
 
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
 @Mixin(SimpleTickScheduler.class)
@@ -18,28 +19,30 @@ public abstract class MixinSimpleTickScheduler implements ICachedChunkTickSchedu
     @Shadow
     public abstract ListTag toNbt();
 
-    private AtomicReference<ListTag> preparedNbt = new AtomicReference<>();
+    private AtomicReference<Optional<ListTag>> preparedNbt = new AtomicReference<>();
 
     @Inject(method = "<init>", at = @At("RETURN"))
     private void onInit(CallbackInfo info) {
         preparedNbt = new AtomicReference<>();
+        prepareCachedNbt(); // Avoid race condition when converting from ProtoChunk to WorldChunk
     }
 
     @Override
     public void prepareCachedNbt() {
         if (preparedNbt == null) preparedNbt = new AtomicReference<>();
-        preparedNbt.set(toNbt());
+        preparedNbt.set(Optional.ofNullable(toNbt()));
     }
 
     @Override
     public ListTag getCachedNbt() {
         if (preparedNbt == null) preparedNbt = new AtomicReference<>();
+        //noinspection OptionalAssignedToNull
         if (preparedNbt.get() == null) {
             new IllegalStateException("Tried to serialize ticklist with no cached nbt! This will affect data integrity. Incompatible mods?").printStackTrace();
             prepareCachedNbt();
         }
-        final ListTag preparedNbt = this.preparedNbt.get();
-        this.preparedNbt = null;
+        final ListTag preparedNbt = this.preparedNbt.get().orElse(null);
+        this.preparedNbt.set(null);
         return preparedNbt;
     }
 }
