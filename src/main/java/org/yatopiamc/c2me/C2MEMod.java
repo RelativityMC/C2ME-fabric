f18d3e8956b65cfe61786b2093ed075086b0ca52
ishland <ishlandmc@yeah.net>
2021-03-06 08:36:56 +0800

    Feature/metrics (#13)

--- src/main/java/org/yatopiamc/c2me/C2MEMod.java
+++ src/main/java/org/yatopiamc/c2me/C2MEMod.java
@@ -1,9 +1,17 @@
 package org.yatopiamc.c2me;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import org.yatopiamc.c2me.common.config.C2MEConfig;
+import org.yatopiamc.c2me.metrics.Metrics;
+
+import java.util.Locale;
 
 public class C2MEMod implements ModInitializer {
     @Override
     public void onInitialize() {
+        final Metrics metrics = new Metrics(10514);
+        metrics.addCustomChart(new Metrics.SimplePie("environmentType", () -> FabricLoader.getInstance().getEnvironmentType().name().toLowerCase(Locale.ENGLISH)));
+        metrics.addCustomChart(new Metrics.SimplePie("useThreadedWorldGeneration", () -> String.valueOf(C2MEConfig.threadedWorldGenConfig.enabled)));
     }
 }
