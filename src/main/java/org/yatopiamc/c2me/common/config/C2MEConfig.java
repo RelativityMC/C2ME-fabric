ea401b138211d9b966aac5f98cd341898a2f2228
ishland <ishlandmc@yeah.net>
2021-04-17 11:55:02 +0800

    Improve configuration system

--- src/main/java/org/yatopiamc/c2me/common/config/C2MEConfig.java
+++ src/main/java/org/yatopiamc/c2me/common/config/C2MEConfig.java
@@ -22,8 +22,10 @@ public class C2MEConfig {
                 .build();
         config.load();
 
-        asyncIoConfig = new AsyncIoConfig(ConfigUtils.getValue(config, "asyncIO", CommentedConfig::inMemory, "Configuration for async io system"));
-        threadedWorldGenConfig = new ThreadedWorldGenConfig(ConfigUtils.getValue(config, "threadedWorldGen", CommentedConfig::inMemory, "Configuration for threaded world generation"));
+        final ConfigUtils.ConfigScope configScope = new ConfigUtils.ConfigScope(config);
+        asyncIoConfig = new AsyncIoConfig(ConfigUtils.getValue(configScope, "asyncIO", CommentedConfig::inMemory, "Configuration for async io system"));
+        threadedWorldGenConfig = new ThreadedWorldGenConfig(ConfigUtils.getValue(configScope, "threadedWorldGen", CommentedConfig::inMemory, "Configuration for threaded world generation"));
+        configScope.removeUnusedKeys();
         config.save();
         config.close();
         C2MEMod.LOGGER.info("Configuration loaded successfully after {}ms", (System.nanoTime() - startTime) / 1_000_000.0);
@@ -35,8 +37,10 @@ public class C2MEConfig {
 
         public AsyncIoConfig(CommentedConfig config) {
             Preconditions.checkNotNull(config, "asyncIo config is not present");
-            this.serializerParallelism = ConfigUtils.getValue(config, "serializerParallelism", () -> Math.min(2, Runtime.getRuntime().availableProcessors()), "IO worker executor parallelism", ConfigUtils.CheckType.THREAD_COUNT);
-            this.ioWorkerParallelism = ConfigUtils.getValue(config, "ioWorkerParallelism", () -> Math.min(6, Runtime.getRuntime().availableProcessors()), "Serializer executor parallelism", ConfigUtils.CheckType.THREAD_COUNT);
+            final ConfigUtils.ConfigScope configScope = new ConfigUtils.ConfigScope(config);
+            this.serializerParallelism = ConfigUtils.getValue(configScope, "serializerParallelism", () -> Math.min(2, Runtime.getRuntime().availableProcessors()), "IO worker executor parallelism", ConfigUtils.CheckType.THREAD_COUNT);
+            this.ioWorkerParallelism = ConfigUtils.getValue(configScope, "ioWorkerParallelism", () -> Math.min(6, Runtime.getRuntime().availableProcessors()), "Serializer executor parallelism", ConfigUtils.CheckType.THREAD_COUNT);
+            configScope.removeUnusedKeys();
         }
     }
 
@@ -47,9 +51,11 @@ public class C2MEConfig {
 
         public ThreadedWorldGenConfig(CommentedConfig config) {
             Preconditions.checkNotNull(config, "threadedWorldGen config is not present");
-            this.enabled = ConfigUtils.getValue(config, "enabled", () -> true, "Whether to enable this feature");
-            this.parallelism = ConfigUtils.getValue(config, "parallelism", () -> Math.min(6, Runtime.getRuntime().availableProcessors()), "World generation worker executor parallelism", ConfigUtils.CheckType.THREAD_COUNT);
-            this.allowThreadedFeatures = ConfigUtils.getValue(config, "allowThreadedFeatures", () -> false, "Whether to allow feature generation (world decorations like trees, ores and etc.) run in parallel");
+            final ConfigUtils.ConfigScope configScope = new ConfigUtils.ConfigScope(config);
+            this.enabled = ConfigUtils.getValue(configScope, "enabled", () -> true, "Whether to enable this feature");
+            this.parallelism = ConfigUtils.getValue(configScope, "parallelism", () -> Math.min(6, Runtime.getRuntime().availableProcessors()), "World generation worker executor parallelism", ConfigUtils.CheckType.THREAD_COUNT);
+            this.allowThreadedFeatures = ConfigUtils.getValue(configScope, "allowThreadedFeatures", () -> false, "Whether to allow feature generation (world decorations like trees, ores and etc.) run in parallel");
+            configScope.removeUnusedKeys();
         }
     }
 
