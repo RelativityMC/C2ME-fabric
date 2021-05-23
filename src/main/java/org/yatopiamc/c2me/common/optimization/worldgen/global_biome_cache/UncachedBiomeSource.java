ec3d78b310640c9cc76fe4970445451dcabd6084
ishland <ishlandmc@yeah.net>
2021-05-23 13:30:36 +0800

    Global biome cache
    Improve world generation speed by ~20%
    Behavior is identical to vanilla

---
+++ src/main/java/org/yatopiamc/c2me/common/optimization/worldgen/global_biome_cache/UncachedBiomeSource.java
@@ -0,0 +1,36 @@
+package org.yatopiamc.c2me.common.optimization.worldgen.global_biome_cache;
+
+import com.mojang.serialization.Codec;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.source.BiomeLayerSampler;
+import net.minecraft.world.biome.source.BiomeSource;
+import net.minecraft.world.biome.source.VanillaLayeredBiomeSource;
+
+import java.util.List;
+
+public class UncachedBiomeSource extends BiomeSource {
+    private final BiomeLayerSampler sampler;
+    private final Registry<Biome> registry;
+
+    public UncachedBiomeSource(List<Biome> biomes, BiomeLayerSampler sampler, Registry<Biome> registry) {
+        super(biomes);
+        this.sampler = sampler;
+        this.registry = registry;
+    }
+
+    @Override
+    protected Codec<? extends BiomeSource> getCodec() {
+        return VanillaLayeredBiomeSource.CODEC;
+    }
+
+    @Override
+    public BiomeSource withSeed(long seed) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Biome getBiomeForNoiseGen(int biomeX, int biomeY, int biomeZ) {
+        return sampler.sample(this.registry, biomeX, biomeZ);
+    }
+}
---
+++ src/main/java/org/yatopiamc/c2me/mixin/optimization/worldgen/global_biome_cache/MixinVanillaLayeredBiomeSource.java
@@ -0,0 +1,58 @@
+package org.yatopiamc.c2me.mixin.optimization.worldgen.global_biome_cache;
+
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.source.BiomeArray;
+import net.minecraft.world.biome.source.BiomeLayerSampler;
+import net.minecraft.world.biome.source.BiomeSource;
+import net.minecraft.world.biome.source.VanillaLayeredBiomeSource;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.yatopiamc.c2me.common.optimization.worldgen.global_biome_cache.BiomeCache;
+import org.yatopiamc.c2me.common.optimization.worldgen.global_biome_cache.IVanillaLayeredBiomeSource;
+
+import java.util.List;
+
+@Mixin(VanillaLayeredBiomeSource.class)
+public abstract class MixinVanillaLayeredBiomeSource extends BiomeSource implements IVanillaLayeredBiomeSource {
+
+    @Shadow @Final private BiomeLayerSampler biomeSampler;
+
+    @Shadow @Final private Registry<Biome> biomeRegistry;
+
+    protected MixinVanillaLayeredBiomeSource(List<Biome> biomes) {
+        super(biomes);
+    }
+
+    private BiomeCache cacheImpl = null;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void onInit(CallbackInfo info) {
+        this.cacheImpl = new BiomeCache(biomeSampler, biomeRegistry, biomes);
+    }
+
+    /**
+     * @author ishland
+     * @reason re-implement caching
+     */
+    @Overwrite
+    public Biome getBiomeForNoiseGen(int biomeX, int biomeY, int biomeZ) {
+        return this.cacheImpl.getBiomeForNoiseGen(biomeX, biomeY, biomeZ);
+    }
+
+    @Override
+    public BiomeArray preloadBiomes(ChunkPos pos, BiomeArray def) {
+        return cacheImpl.preloadBiomes(pos, def);
+    }
+
+    @Override
+    public BiomeArray getBiomes(ChunkPos pos) {
+        return cacheImpl.preloadBiomes(pos, null);
+    }
+}
