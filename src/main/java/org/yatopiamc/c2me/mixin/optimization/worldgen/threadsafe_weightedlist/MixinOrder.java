f77cb8fd2442810a64f0611b48a0ac8f532d9a4e
ishland <ishlandmc@yeah.net>
2021-06-15 22:09:50 +0800

    Better fix for https://github.com/YatopiaMC/C2ME-fabric/issues/31

--- src/main/java/org/yatopiamc/c2me/mixin/optimization/worldgen/threadsafe_weightedlist/MixinOrder.java
+++ src/main/java/org/yatopiamc/c2me/mixin/optimization/worldgen/threadsafe_weightedlist/MixinOrder.java
@@ -2,19 +2,27 @@ package org.yatopiamc.c2me.mixin.optimization.worldgen.threadsafe_weightedlist;
 
 import net.minecraft.entity.ai.brain.task.CompositeTask;
 import net.minecraft.util.collection.WeightedList;
-import org.spongepowered.asm.mixin.Dynamic;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.yatopiamc.c2me.common.optimization.worldgen.threadsafe_weightedlist.IWeightedList;
+
+import java.util.function.Consumer;
 
 @Mixin(CompositeTask.Order.class)
 public class MixinOrder {
 
-    @Dynamic
-    @Redirect(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/collection/WeightedList;shuffle()Lnet/minecraft/util/collection/WeightedList;"))
-    private WeightedList<?> redirectShuffle(WeightedList<?> obj) {
-        return ((IWeightedList<?>) obj).shuffleVanilla();
+    @Mutable
+    @Shadow @Final private Consumer<WeightedList<?>> listModifier;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void onInit(String enumName, int ordinal, Consumer<WeightedList<?>> listModifier, CallbackInfo ci) {
+        if (enumName.equals("field_18349") || enumName.equals("SHUFFLED"))
+            this.listModifier = obj -> ((IWeightedList<?>) obj).shuffleVanilla();
     }
 
 }
