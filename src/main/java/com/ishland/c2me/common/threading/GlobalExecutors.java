99c4b515882a862121653f84702a668248449372
ishland <ishlandmc@yeah.net>
2021-06-20 13:59:20 +0800

    Cleanup code

--- src/main/java/com/ishland/c2me/common/threading/GlobalExecutors.java
+++ src/main/java/com/ishland/c2me/common/threading/GlobalExecutors.java
@@ -2,24 +2,12 @@ package com.ishland.c2me.common.threading;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class GlobalExecutors {
 
-    public static final ScheduledThreadPoolExecutor scheduler = new ScheduledThreadPoolExecutor(
-            1,
-            new ThreadFactoryBuilder().setNameFormat("C2ME scheduler").setDaemon(true).setPriority(Thread.NORM_PRIORITY - 1).setThreadFactory(r -> {
-                final Thread thread = new Thread(r);
-                GlobalExecutors.schedulerThread.set(thread);
-                return thread;
-            }).build()
-    );
-    private static final AtomicReference<Thread> schedulerThread = new AtomicReference<>();
+    public static final ExecutorService scheduler = Executors.newSingleThreadExecutor(new ThreadFactoryBuilder().setNameFormat("C2ME scheduler").setDaemon(true).setPriority(Thread.NORM_PRIORITY).build());
 
-    public static void ensureSchedulerThread() {
-        if (Thread.currentThread() != schedulerThread.get())
-            throw new IllegalStateException("Not on scheduler thread");
-    }
 
 }
