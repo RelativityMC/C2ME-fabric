3d5be3402af687f46d8874506785cd4f6f0f65fd
ishland <ishlandmc@yeah.net>
2021-06-20 11:17:40 +0800

    Metrics changes

--- src/main/java/com/ishland/c2me/mixin/metrics/MixinMinecraftServer.java
+++ src/main/java/com/ishland/c2me/mixin/metrics/MixinMinecraftServer.java
@@ -3,30 +3,31 @@ package com.ishland.c2me.mixin.metrics;
 import com.google.common.collect.ImmutableMap;
 import com.ishland.c2me.common.config.C2MEConfig;
 import com.ishland.c2me.metrics.Metrics;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.MinecraftVersion;
 import net.minecraft.server.MinecraftServer;
-import org.apache.logging.log4j.Logger;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.server.dedicated.MinecraftDedicatedServer;
+import net.minecraft.server.integrated.IntegratedServer;
+import net.minecraft.test.TestServer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Locale;
-
 @Mixin(MinecraftServer.class)
 public class MixinMinecraftServer {
 
-    @Shadow
-    @Final
-    static Logger LOGGER;
 
+    @SuppressWarnings("ConstantConditions")
     @Inject(method = "runServer", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/server/MinecraftServer;setupServer()Z"))
     private void onInit(CallbackInfo info) {
         final Metrics metrics = new Metrics(10514, (MinecraftServer) (Object) this);
         metrics.addCustomChart(new Metrics.SimplePie("useThreadedWorldGeneration", () -> String.valueOf(C2MEConfig.threadedWorldGenConfig.enabled)));
+        metrics.addCustomChart(new Metrics.SimplePie("serverType", () -> {
+            if ((Object) this instanceof MinecraftDedicatedServer) return "Dedicated Server";
+            if ((Object) this instanceof IntegratedServer) return "Integrated Server";
+            if ((Object) this instanceof TestServer) return "Test Server";
+            return "Unknown: " + this.getClass().getName();
+        }));
         if (C2MEConfig.threadedWorldGenConfig.enabled) {
             metrics.addCustomChart(new Metrics.SimplePie("useThreadedWorldFeatureGeneration", () -> String.valueOf(C2MEConfig.threadedWorldGenConfig.allowThreadedFeatures)));
             metrics.addCustomChart(new Metrics.SimplePie("useReducedLockRadius", () -> String.valueOf(C2MEConfig.threadedWorldGenConfig.reduceLockRadius)));
