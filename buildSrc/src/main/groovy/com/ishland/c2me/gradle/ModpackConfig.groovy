8ecbb0c9d968f9ebd42cf356e848b55f168b5135
ishland <ishlandmc@yeah.net>
2021-08-08 21:46:35 +0800

    Revert "Download repurposed structures download from modrinth"
    This reverts commit a924e28e1b099b91bd9dad9060586dae73508329.

--- buildSrc/src/main/groovy/com/ishland/c2me/gradle/ModpackConfig.groovy
+++ buildSrc/src/main/groovy/com/ishland/c2me/gradle/ModpackConfig.groovy
@@ -4,12 +4,12 @@ class ModpackConfig {
 
     static def applyModpack(handler) {
         // mod dependencies
-        handler.modImplementation("net.fabricmc.fabric-api:fabric-api:0.37.1+1.17")
+        handler.modImplementation("net.fabricmc.fabric-api:fabric-api:0.37.1+1.17") // https://fabricmc.net/versions.html
         handler.modImplementation("me.shedaniel.cloth:cloth-config-fabric:5.0.34") // https://www.curseforge.com/minecraft/mc-mods/cloth-config
         handler.modImplementation("com.github.Draylar.omega-config:omega-config-base:1.0.8-1.17") // https://github.com/Draylar/omega-config
 
         // mods
-        handler.modImplementation("maven.modrinth:repurposed-structures-fabric:2.1.11+1.17.1") // https://modrinth.com/mod/repurposed-structures-fabric
+        handler.modImplementation("com.telepathicgrunt:RepurposedStructures-Fabric:2.1.11+1.17.1") { transitive false } // https://github.com/TelepathicGrunt/RepurposedStructures-Fabric
     }
 
 }
