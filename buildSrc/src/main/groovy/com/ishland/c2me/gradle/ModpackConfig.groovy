db4a6eaf8e0609c5e154ec0bc54afe05b1bd0db2
ishland <ishlandmc@yeah.net>
2021-08-08 22:53:22 +0800

    Updates workflow and build file

--- buildSrc/src/main/groovy/com/ishland/c2me/gradle/ModpackConfig.groovy
+++ buildSrc/src/main/groovy/com/ishland/c2me/gradle/ModpackConfig.groovy
@@ -5,11 +5,12 @@ class ModpackConfig {
     static def applyModpack(handler) {
         // mod dependencies
         handler.modImplementation("net.fabricmc.fabric-api:fabric-api:0.37.1+1.17") // https://fabricmc.net/versions.html
-        handler.modImplementation("me.shedaniel.cloth:cloth-config-fabric:5.0.34") // https://www.curseforge.com/minecraft/mc-mods/cloth-config
-        handler.modImplementation("com.github.Draylar.omega-config:omega-config-base:1.0.8-1.17") // https://github.com/Draylar/omega-config
 
         // mods
-        handler.modImplementation("com.telepathicgrunt:RepurposedStructures-Fabric:2.1.11+1.17.1") { transitive false } // https://github.com/TelepathicGrunt/RepurposedStructures-Fabric
+        handler.modImplementation("com.telepathicgrunt:RepurposedStructures-Fabric:2.1.11+1.17.1") {
+            exclude(group: "net.fabricmc.fabric-api")
+            exclude(group: "maven.modrinth", module: "modmenu")
+        } // https://github.com/TelepathicGrunt/RepurposedStructures-Fabric
     }
 
 }
