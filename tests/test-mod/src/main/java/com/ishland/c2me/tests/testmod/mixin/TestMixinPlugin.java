da7ff154d043b193ea38967edbeba080e9349b64
ishland <ishlandmc@yeah.net>
2021-08-30 15:39:54 +0800

    Fix weird remapper issue

---
+++ tests/test-mod/src/main/java/com/ishland/c2me/tests/testmod/mixin/TestMixinPlugin.java
@@ -0,0 +1,48 @@
+package com.ishland.c2me.tests.testmod.mixin;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class TestMixinPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (mixinClassName.startsWith("com.ishland.c2me.tests.testmod.mixin.fix.remapper_being_broken."))
+            return FabricLoader.getInstance().isDevelopmentEnvironment();
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+}
