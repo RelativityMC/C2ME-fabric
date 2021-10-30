27fb26b7ad6a142094c76d7e9ef8dbd08351039e
ishland <ishlandmc@yeah.net>
2021-10-30 17:22:34 +0800

    Throughput improvements for async scheduling

--- src/main/java/com/ishland/c2me/mixin/access/IThreadedAnvilChunkStorage.java
+++ src/main/java/com/ishland/c2me/mixin/access/IThreadedAnvilChunkStorage.java
@@ -1,11 +1,10 @@
 package com.ishland.c2me.mixin.access;
 
-import it.unimi.dsi.fastutil.longs.Long2ObjectLinkedOpenHashMap;
-import net.minecraft.server.world.ChunkHolder;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.server.world.ThreadedAnvilChunkStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(ThreadedAnvilChunkStorage.class)
 public interface IThreadedAnvilChunkStorage {
@@ -13,7 +12,7 @@ public interface IThreadedAnvilChunkStorage {
     @Accessor
     ServerWorld getWorld();
 
-    @Accessor
-    Long2ObjectLinkedOpenHashMap<ChunkHolder> getChunkHolders();
+    @Invoker
+    boolean invokeUpdateHolderMap();
 
 }
--- src/main/java/com/ishland/c2me/mixin/threading/async_scheduling/MixinThreadedAnvilChunkStorage.java
+++ src/main/java/com/ishland/c2me/mixin/threading/async_scheduling/MixinThreadedAnvilChunkStorage.java
@@ -1,8 +1,8 @@
 package com.ishland.c2me.mixin.threading.async_scheduling;
 
-import com.ibm.asyncutil.util.StageSupport;
 import com.ishland.c2me.common.GlobalExecutors;
 import com.mojang.datafixers.util.Either;
+import it.unimi.dsi.fastutil.longs.Long2ObjectLinkedOpenHashMap;
 import net.minecraft.server.world.ChunkHolder;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.server.world.ThreadedAnvilChunkStorage;
@@ -13,6 +13,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.concurrent.CompletableFuture;
@@ -25,15 +26,20 @@ public abstract class MixinThreadedAnvilChunkStorage {
 
     @Shadow protected abstract CompletableFuture<Either<Chunk, ChunkHolder.Unloaded>> upgradeChunk(ChunkHolder holder, ChunkStatus requiredStatus);
 
+    @Shadow private volatile Long2ObjectLinkedOpenHashMap<ChunkHolder> chunkHolders;
+
     @Inject(method = "upgradeChunk", at = @At(value = "HEAD"), cancellable = true)
     private void beforeUpgradeChunk(ChunkHolder holder, ChunkStatus requiredStatus, CallbackInfoReturnable<CompletableFuture<Either<Chunk, ChunkHolder.Unloaded>>> cir) {
         if (this.world.getServer().getThread() == Thread.currentThread()) {
             cir.setReturnValue(
-                    StageSupport.tryComposeWith(GlobalExecutors.schedulingLock.acquireLock(), unused ->
-                            CompletableFuture.supplyAsync(() -> this.upgradeChunk(holder, requiredStatus), GlobalExecutors.executor)
-                    ).thenCompose(Function.identity()).toCompletableFuture()
+                    CompletableFuture.supplyAsync(() -> this.upgradeChunk(holder, requiredStatus), GlobalExecutors.asyncScheduler).thenCompose(Function.identity())
             );
         }
+    }
+
+    @Redirect(method = "getRegion", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ThreadedAnvilChunkStorage;getCurrentChunkHolder(J)Lnet/minecraft/server/world/ChunkHolder;"))
+    private ChunkHolder redirectGetChunkHolder(ThreadedAnvilChunkStorage instance, long pos) {
+        return this.chunkHolders.get(pos); // thread-safe
     }
 
 }
