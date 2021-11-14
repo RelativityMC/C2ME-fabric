2702c36e998fdb53fa927c556cf665f47926c4cd
Zoë <duplexsys@protonmail.com>
2021-11-13 21:09:28 -0600

    fix error

--- src/main/java/com/ishland/c2me/common/config/C2MEConfig.java
+++ src/main/java/com/ishland/c2me/common/config/C2MEConfig.java
@@ -100,7 +100,7 @@ public class C2MEConfig {
             this.enabled = ConfigUtils.getValue(configScope, "enabled", () -> getDefaultGlobalExecutorParallelism() >= 3 || global_enabled, "Whether to enable this feature", List.of(), false, true);
             this.allowThreadedFeatures = ConfigUtils.getValue(configScope, "allowThreadedFeatures", () -> true, "Whether to allow feature generation (world decorations like trees, ores and etc.) run in parallel \n (may cause incompatibility with other mods)", List.of(), null, true);
             this.reduceLockRadius = ConfigUtils.getValue(configScope, "reduceLockRadius", () -> false || global_reduceLockRadius, "Whether to allow reducing lock radius \n (may cause incompatibility with other mods)", List.of(), null, true);
-            this.useGlobalBiomeCache = ConfigUtils.getValue(configScope, "useGlobalBiomeCache", () -> false || global_useGlobalBiomeCache, "(DO NOT USE in 1.18) \n Whether to enable global BiomeCache to accelerate worldgen \n This increases memory allocation ", List.of(), false, true);
+            this.useGlobalBiomeCache = ConfigUtils.getValue(configScope, "useGlobalBiomeCache", () -> false, "(DO NOT USE in 1.18) \n Whether to enable global BiomeCache to accelerate worldgen \n This increases memory allocation ", List.of(), false, true);
             configScope.removeUnusedKeys();
         }
     }
