2eb58d9987e713799e24feef96822deab9ca9c3f
ishland <ishlandmc@yeah.net>
2021-11-20 16:58:52 +0800

    Fix a threading issue

---
+++ src/main/java/com/ishland/c2me/mixin/fixes/worldgen/threading/MixinStructureChecker.java
@@ -0,0 +1,58 @@
+package com.ishland.c2me.mixin.fixes.worldgen.threading;
+
+import it.unimi.dsi.fastutil.longs.Long2BooleanMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectMaps;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectMaps;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import net.minecraft.class_6832;
+import net.minecraft.world.gen.feature.StructureFeature;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.function.Consumer;
+
+@Mixin(class_6832.class)
+public class MixinStructureChecker {
+
+    @Mutable
+    @Shadow @Final private Long2ObjectMap<Object2IntMap<StructureFeature<?>>> field_36237;
+
+    @Mutable
+    @Shadow @Final private Map<StructureFeature<?>, Long2BooleanMap> field_36238;
+
+    @Unique
+    private Object mapMutex = new Object();
+
+    @Inject(method = "<init>", at = @At(value = "RETURN"))
+    private void onInit(CallbackInfo info) {
+        this.mapMutex = new Object();
+        this.field_36237 = Long2ObjectMaps.synchronize(this.field_36237);
+        this.field_36238 = Object2ObjectMaps.synchronize(new Object2ObjectOpenHashMap<>(), this.mapMutex);
+    }
+
+    @Redirect(method = "method_39828", at = @At(value = "INVOKE", target = "Ljava/util/Collection;forEach(Ljava/util/function/Consumer;)V"))
+    private void redirectForEach(Collection<Long2BooleanMap> instance, Consumer<Long2BooleanMap> consumer) {
+        //noinspection SynchronizeOnNonFinalField
+        synchronized (this.mapMutex) {
+            final Iterator<Long2BooleanMap> iterator = instance.iterator();
+            while (iterator.hasNext()) {
+                final Long2BooleanMap next = iterator.next();
+                consumer.accept(next);
+                if (next.isEmpty()) iterator.remove();
+            }
+        }
+    }
+
+}
