6385a49b5df402e3b93e3f2b977ed9e0aa63939d
Altirix <19439141+Altirix@users.noreply.github.com>
2021-12-09 01:15:06 +0000

    max render distance modify with setmax

--- src/main/java/com/ishland/c2me/client/mixin/uncapvd/MixinOption.java
+++ src/main/java/com/ishland/c2me/client/mixin/uncapvd/MixinOption.java
@@ -5,9 +5,13 @@ import net.minecraft.client.option.DoubleOption;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.option.Option;
 import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
@@ -16,9 +20,11 @@ import java.util.function.Function;
 @Mixin(Option.class)
 public class MixinOption {
 
-    @ModifyArg(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/DoubleOption;<init>(Ljava/lang/String;DDFLjava/util/function/Function;Ljava/util/function/BiConsumer;Ljava/util/function/BiFunction;)V"), index = 2)
-    private static double modifyMaxViewDistance(String key, double min, double max, float step, Function<GameOptions, Double> getter, BiConsumer<GameOptions, Double> setter, BiFunction<GameOptions, DoubleOption, Text> displayStringGetter) {
-        return C2MEConfig.clientSideConfig.modifyMaxVDConfig.maxViewDistance;
+    @Shadow @Final public static DoubleOption RENDER_DISTANCE;
+
+    @Inject(method = "<clinit>", at = @At(value = "TAIL"))
+    private static void modifyMaxViewDistance(CallbackInfo ci) {
+        RENDER_DISTANCE.setMax(C2MEConfig.clientSideConfig.modifyMaxVDConfig.maxViewDistance);
     }
 
 }
