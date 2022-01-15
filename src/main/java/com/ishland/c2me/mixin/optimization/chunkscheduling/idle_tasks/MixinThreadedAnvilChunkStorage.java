99b94e248f2c7f4bac02c40ee4ec5f15ef815f51
ishland <ishlandmc@yeah.net>
2022-01-15 22:14:05 +0800

    Optimize autosave a bit

--- src/main/java/com/ishland/c2me/mixin/optimization/chunkscheduling/idle_tasks/MixinThreadedAnvilChunkStorage.java
+++ src/main/java/com/ishland/c2me/mixin/optimization/chunkscheduling/idle_tasks/MixinThreadedAnvilChunkStorage.java
@@ -2,6 +2,7 @@ package com.ishland.c2me.mixin.optimization.chunkscheduling.idle_tasks;
 
 import com.ishland.c2me.common.optimization.chunkscheduling.idle_tasks.IThreadedAnvilChunkStorage;
 import it.unimi.dsi.fastutil.longs.Long2ObjectLinkedOpenHashMap;
+import it.unimi.dsi.fastutil.objects.ObjectLinkedOpenHashSet;
 import net.minecraft.server.world.ChunkHolder;
 import net.minecraft.server.world.ThreadedAnvilChunkStorage;
 import net.minecraft.util.math.ChunkPos;
@@ -13,7 +14,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Mixin(ThreadedAnvilChunkStorage.class)
@@ -30,7 +30,7 @@ public abstract class MixinThreadedAnvilChunkStorage implements IThreadedAnvilCh
     private final ConcurrentLinkedQueue<ChunkPos> dirtyChunkPosForAutoSave = new ConcurrentLinkedQueue<>();
 
     @Unique
-    private final LinkedList<ChunkHolder> dirtyChunkHoldersForAutoSave = new LinkedList<>();
+    private final ObjectLinkedOpenHashSet<ChunkHolder> dirtyChunkHoldersForAutoSave = new ObjectLinkedOpenHashSet<>();
 
     @Redirect(method = "unloadChunks", at = @At(value = "FIELD", target = "Lnet/minecraft/server/world/ThreadedAnvilChunkStorage;chunkHolders:Lit/unimi/dsi/fastutil/longs/Long2ObjectLinkedOpenHashMap;"))
     private Long2ObjectLinkedOpenHashMap<ChunkHolder> stopAutoSaveInUnloading(ThreadedAnvilChunkStorage instance) {
