c8f4f075a761adfe58b44a6d957ab97c0547544a
ishland <ishlandmc@yeah.net>
2022-01-16 20:22:11 +0800

    Restore 1.18.1 mobcaps vanilla behavior in no-tick chunks
    MSPT goes from 74 to 52 when 262144 entities are in no-tick chunks

--- src/main/java/com/ishland/c2me/mixin/notickvd/MixinServerChunkManager.java
+++ src/main/java/com/ishland/c2me/mixin/notickvd/MixinServerChunkManager.java
@@ -1,18 +1,10 @@
 package com.ishland.c2me.mixin.notickvd;
 
-import com.ishland.c2me.common.notickvd.IChunkTicketManager;
-import com.ishland.c2me.common.util.FilteringIterable;
-import com.ishland.c2me.common.util.MCUtil;
-import it.unimi.dsi.fastutil.longs.LongSet;
-import net.minecraft.entity.Entity;
 import net.minecraft.server.world.ChunkTicketManager;
 import net.minecraft.server.world.ServerChunkManager;
-import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ServerChunkManager.class)
 public class MixinServerChunkManager {
@@ -26,11 +18,11 @@ public class MixinServerChunkManager {
 //    }
 
 
-    @Redirect(method = "tickChunks", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;iterateEntities()Ljava/lang/Iterable;"))
-    private Iterable<Entity> redirectIterateEntities(ServerWorld serverWorld) {
-        final LongSet noTickOnlyChunks = ((IChunkTicketManager) this.ticketManager).getNoTickOnlyChunks();
-        if (noTickOnlyChunks == null) return serverWorld.iterateEntities();
-        return new FilteringIterable<>(serverWorld.iterateEntities(), entity -> !noTickOnlyChunks.contains(MCUtil.toLong(entity.getChunkPos())));
-    }
+//    @Redirect(method = "tickChunks", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;iterateEntities()Ljava/lang/Iterable;"))
+//    private Iterable<Entity> redirectIterateEntities(ServerWorld serverWorld) {
+//        final LongSet noTickOnlyChunks = ((IChunkTicketManager) this.ticketManager).getNoTickOnlyChunks();
+//        if (noTickOnlyChunks == null) return serverWorld.iterateEntities();
+//        return new FilteringIterable<>(serverWorld.iterateEntities(), entity -> !noTickOnlyChunks.contains(MCUtil.toLong(entity.getChunkPos())));
+//    }
 
 }
