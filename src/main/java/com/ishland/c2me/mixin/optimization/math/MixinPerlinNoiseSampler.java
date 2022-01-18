7107e0afeb0ffb708ff4ea5167fc5509bff9ed57
ishland <ishlandmc@yeah.net>
2022-01-18 21:36:37 +0800

    Revert "Cache some math"
    This reverts commit 39a5697e5d4e40587b79230f59567ad22f230b20.
    
    Looks like some bandwidth issue causing issues with performance.

--- src/main/java/com/ishland/c2me/mixin/optimization/math/MixinPerlinNoiseSampler.java
+++ src/main/java/com/ishland/c2me/mixin/optimization/math/MixinPerlinNoiseSampler.java
@@ -6,9 +6,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = PerlinNoiseSampler.class, priority = 1090)
 public abstract class MixinPerlinNoiseSampler {
@@ -40,21 +37,6 @@ public abstract class MixinPerlinNoiseSampler {
             {-1, 1, 0},
             {0, -1, -1}
     };
-
-    @Unique
-    private int[] permutationsInt = new int[0];
-    @Unique
-    private int[] permutationsIntAnd15 = new int[0];
-
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void onInit(CallbackInfo info) {
-        this.permutationsInt = new int[this.permutations.length];
-        this.permutationsIntAnd15 = new int[this.permutations.length];
-        for (int i = 0; i < this.permutations.length; i++) {
-            this.permutationsInt[i] = this.permutations[i] & 0xFF;
-            this.permutationsIntAnd15[i] = this.permutations[i] & 15;
-        }
-    }
 
     /**
      * @author ishland
@@ -89,23 +71,23 @@ public abstract class MixinPerlinNoiseSampler {
 
     /**
      * @author ishland
-     * @reason inline math & small optimization: remove frequent type conversions, cache and reorder
+     * @reason inline math & small optimization: remove frequent type conversions and redundant ops
      */
     @Overwrite
     private double sample(int sectionX, int sectionY, int sectionZ, double localX, double localY, double localZ, double fadeLocalX) {
-        // TODO [VanillaCopy] but heavily modified
+        // TODO [VanillaCopy] but inlined
         final int var0 = sectionX & 0xFF;
         final int var1 = (sectionX + 1) & 0xFF;
-        final int var2 = this.permutationsInt[var0];
-        final int var3 = this.permutationsInt[var1];
+        final int var2 = this.permutations[var0] & 0xFF;
+        final int var3 = this.permutations[var1] & 0xFF;
         final int var4 = (var2 + sectionY) & 0xFF;
         final int var5 = (var2 + sectionY + 1) & 0xFF;
         final int var6 = (var3 + sectionY) & 0xFF;
         final int var7 = (var3 + sectionY + 1) & 0xFF;
-        final int var8 = this.permutationsInt[var4];
-        final int var9 = this.permutationsInt[var5];
-        final int var10 = this.permutationsInt[var6];
-        final int var11 = this.permutationsInt[var7];
+        final int var8 = this.permutations[var4] & 0xFF;
+        final int var9 = this.permutations[var5] & 0xFF;
+        final int var10 = this.permutations[var6] & 0xFF;
+        final int var11 = this.permutations[var7] & 0xFF;
 
         final int var12 = (var8 + sectionZ) & 0xFF;
         final int var13 = (var10 + sectionZ) & 0xFF;
@@ -115,14 +97,14 @@ public abstract class MixinPerlinNoiseSampler {
         final int var17 = (var10 + sectionZ + 1) & 0xFF;
         final int var18 = (var9 + sectionZ + 1) & 0xFF;
         final int var19 = (var11 + sectionZ + 1) & 0xFF;
-        final int var20 = this.permutationsIntAnd15[var12];
-        final int var21 = this.permutationsIntAnd15[var13];
-        final int var22 = this.permutationsIntAnd15[var14];
-        final int var23 = this.permutationsIntAnd15[var15];
-        final int var24 = this.permutationsIntAnd15[var16];
-        final int var25 = this.permutationsIntAnd15[var17];
-        final int var26 = this.permutationsIntAnd15[var18];
-        final int var27 = this.permutationsIntAnd15[var19];
+        final int var20 = this.permutations[var12] & 15;
+        final int var21 = this.permutations[var13] & 15;
+        final int var22 = this.permutations[var14] & 15;
+        final int var23 = this.permutations[var15] & 15;
+        final int var24 = this.permutations[var16] & 15;
+        final int var25 = this.permutations[var17] & 15;
+        final int var26 = this.permutations[var18] & 15;
+        final int var27 = this.permutations[var19] & 15;
         final double[] var28 = SIMPLEX_NOISE_GRADIENTS[var20];
         final double[] var29 = SIMPLEX_NOISE_GRADIENTS[var21];
         final double[] var30 = SIMPLEX_NOISE_GRADIENTS[var22];
