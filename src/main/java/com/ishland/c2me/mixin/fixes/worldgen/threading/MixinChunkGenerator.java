fad301f60b89df0fc3eb25f5afdd632829d1877b
ishland <ishlandmc@yeah.net>
2022-01-20 18:15:20 +0800

    Fix logspam when no strongholds needed

--- src/main/java/com/ishland/c2me/mixin/fixes/worldgen/threading/MixinChunkGenerator.java
+++ src/main/java/com/ishland/c2me/mixin/fixes/worldgen/threading/MixinChunkGenerator.java
@@ -53,55 +53,54 @@ public abstract class MixinChunkGenerator {
     @Overwrite
     private void generateStrongholdPositions() {
         final List<ChunkPos> strongholds = this.strongholds;
-        if (strongholds.isEmpty()) {
+        StrongholdConfig strongholdConfig = this.structuresConfig.getStronghold();
+        if (strongholds.isEmpty() && strongholdConfig != null && strongholdConfig.getCount() != 0) {
             //noinspection SynchronizationOnLocalVariableOrMethodParameter
             synchronized (strongholds) {
                 if (strongholds.isEmpty()) {
                     if (this.strongholds.isEmpty()) {
                         System.out.println("Initializing stronghold positions, this may take a while");
                         // TODO [VanillaCopy]
-                        StrongholdConfig strongholdConfig = this.structuresConfig.getStronghold();
-                        if (strongholdConfig != null && strongholdConfig.getCount() != 0) {
-                            List<Biome> list = Lists.<Biome>newArrayList();
+//                        StrongholdConfig strongholdConfig = this.structuresConfig.getStronghold();
+                        List<Biome> list = Lists.<Biome>newArrayList();
 
-                            for(Biome biome : this.populationSource.getBiomes()) {
-                                if (canPlaceStrongholdInBiome(biome)) {
-                                    list.add(biome);
-                                }
+                        for(Biome biome : this.populationSource.getBiomes()) {
+                            if (canPlaceStrongholdInBiome(biome)) {
+                                list.add(biome);
+                            }
+                        }
+
+                        int i = strongholdConfig.getDistance();
+                        int biome = strongholdConfig.getCount();
+                        int j = strongholdConfig.getSpread();
+                        Random random = new Random();
+                        random.setSeed(this.worldSeed);
+                        double d = random.nextDouble() * Math.PI * 2.0;
+                        int k = 0;
+                        int l = 0;
+
+                        for(int m = 0; m < biome; ++m) {
+                            double e = (double)(4 * i + i * l * 6) + (random.nextDouble() - 0.5) * (double)i * 2.5;
+                            int n = (int)Math.round(Math.cos(d) * e);
+                            int o = (int)Math.round(Math.sin(d) * e);
+                            BlockPos blockPos = this.populationSource
+                                    .locateBiome(
+                                            ChunkSectionPos.getOffsetPos(n, 8), 0, ChunkSectionPos.getOffsetPos(o, 8), 112, list::contains, random, this.getMultiNoiseSampler()
+                                    );
+                            if (blockPos != null) {
+                                n = ChunkSectionPos.getSectionCoord(blockPos.getX());
+                                o = ChunkSectionPos.getSectionCoord(blockPos.getZ());
                             }
 
-                            int i = strongholdConfig.getDistance();
-                            int biome = strongholdConfig.getCount();
-                            int j = strongholdConfig.getSpread();
-                            Random random = new Random();
-                            random.setSeed(this.worldSeed);
-                            double d = random.nextDouble() * Math.PI * 2.0;
-                            int k = 0;
-                            int l = 0;
-
-                            for(int m = 0; m < biome; ++m) {
-                                double e = (double)(4 * i + i * l * 6) + (random.nextDouble() - 0.5) * (double)i * 2.5;
-                                int n = (int)Math.round(Math.cos(d) * e);
-                                int o = (int)Math.round(Math.sin(d) * e);
-                                BlockPos blockPos = this.populationSource
-                                        .locateBiome(
-                                                ChunkSectionPos.getOffsetPos(n, 8), 0, ChunkSectionPos.getOffsetPos(o, 8), 112, list::contains, random, this.getMultiNoiseSampler()
-                                        );
-                                if (blockPos != null) {
-                                    n = ChunkSectionPos.getSectionCoord(blockPos.getX());
-                                    o = ChunkSectionPos.getSectionCoord(blockPos.getZ());
-                                }
-
-                                this.strongholds.add(new ChunkPos(n, o));
-                                d += Math.PI * 2 / (double)j;
-                                ++k;
-                                if (k == j) {
-                                    ++l;
-                                    k = 0;
-                                    j += 2 * j / (l + 1);
-                                    j = Math.min(j, biome - m);
-                                    d += random.nextDouble() * Math.PI * 2.0;
-                                }
+                            this.strongholds.add(new ChunkPos(n, o));
+                            d += Math.PI * 2 / (double)j;
+                            ++k;
+                            if (k == j) {
+                                ++l;
+                                k = 0;
+                                j += 2 * j / (l + 1);
+                                j = Math.min(j, biome - m);
+                                d += random.nextDouble() * Math.PI * 2.0;
                             }
                         }
                         System.out.println("Stronghold positions initialized");
