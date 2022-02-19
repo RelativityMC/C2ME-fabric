9916ec2a7775932717793da54acb4f0cca934b70
ishland <ishlandmc@yeah.net>
2022-02-19 23:13:02 +0800

    Noise optimizations
    Micro optimization for octave perlin noise sampler
    
    More small optimizations on perlin noise
    
    Flatten simplex noise gradients to further optimize perlin noise

--- src/jmh/java/noise/PerlinNoiseBenchmark.java
+++ src/jmh/java/noise/PerlinNoiseBenchmark.java
@@ -21,23 +21,23 @@ public class PerlinNoiseBenchmark {
     private final PerlinNoiseSampler vanillaSampler = new PerlinNoiseSampler(new SimpleRandom(0xFF));
 
     private final byte[] permutations;
-    private static final double[][] SIMPLEX_NOISE_GRADIENTS = new double[][]{
-            {1, 1, 0},
-            {-1, 1, 0},
-            {1, -1, 0},
-            {-1, -1, 0},
-            {1, 0, 1},
-            {-1, 0, 1},
-            {1, 0, -1},
-            {-1, 0, -1},
-            {0, 1, 1},
-            {0, -1, 1},
-            {0, 1, -1},
-            {0, -1, -1},
-            {1, 1, 0},
-            {0, -1, 1},
-            {-1, 1, 0},
-            {0, -1, -1}
+    private static final double[] FLAT_SIMPLEX_GRAD = new double[]{
+            1, 1, 0, 0,
+            -1, 1, 0, 0,
+            1, -1, 0, 0,
+            -1, -1, 0, 0,
+            1, 0, 1, 0,
+            -1, 0, 1, 0,
+            1, 0, -1, 0,
+            -1, 0, -1, 0,
+            0, 1, 1, 0,
+            0, -1, 1, 0,
+            0, 1, -1, 0,
+            0, -1, -1, 0,
+            1, 1, 0, 0,
+            0, -1, 1, 0,
+            -1, 1, 0, 0,
+            0, -1, -1, 0,
     };
     private final double originX = vanillaSampler.originX;
     private final double originY = vanillaSampler.originY;
@@ -90,57 +90,25 @@ public class PerlinNoiseBenchmark {
         final int var17 = (var10 + sectionZ + 1) & 0xFF;
         final int var18 = (var9 + sectionZ + 1) & 0xFF;
         final int var19 = (var11 + sectionZ + 1) & 0xFF;
-        final int var20 = this.permutations[var12] & 15;
-        final int var21 = this.permutations[var13] & 15;
-        final int var22 = this.permutations[var14] & 15;
-        final int var23 = this.permutations[var15] & 15;
-        final int var24 = this.permutations[var16] & 15;
-        final int var25 = this.permutations[var17] & 15;
-        final int var26 = this.permutations[var18] & 15;
-        final int var27 = this.permutations[var19] & 15;
-        final double[] var28 = SIMPLEX_NOISE_GRADIENTS[var20];
-        final double[] var29 = SIMPLEX_NOISE_GRADIENTS[var21];
-        final double[] var30 = SIMPLEX_NOISE_GRADIENTS[var22];
-        final double[] var31 = SIMPLEX_NOISE_GRADIENTS[var23];
-        final double[] var32 = SIMPLEX_NOISE_GRADIENTS[var24];
-        final double[] var33 = SIMPLEX_NOISE_GRADIENTS[var25];
-        final double[] var34 = SIMPLEX_NOISE_GRADIENTS[var26];
-        final double[] var35 = SIMPLEX_NOISE_GRADIENTS[var27];
-        final double var36 = var28[0];
-        final double var37 = var28[1];
-        final double var38 = var28[2];
-        final double var39 = var29[0];
-        final double var40 = var29[1];
-        final double var41 = var29[2];
-        final double var42 = var30[0];
-        final double var43 = var30[1];
-        final double var44 = var30[2];
-        final double var45 = var31[0];
-        final double var46 = var31[1];
-        final double var47 = var31[2];
-        final double var48 = var32[0];
-        final double var49 = var32[1];
-        final double var50 = var32[2];
-        final double var51 = var33[0];
-        final double var52 = var33[1];
-        final double var53 = var33[2];
-        final double var54 = var34[0];
-        final double var55 = var34[1];
-        final double var56 = var34[2];
-        final double var57 = var35[0];
-        final double var58 = var35[1];
-        final double var59 = var35[2];
+        final int var20 = (this.permutations[var12] & 15) << 2;
+        final int var21 = (this.permutations[var13] & 15) << 2;
+        final int var22 = (this.permutations[var14] & 15) << 2;
+        final int var23 = (this.permutations[var15] & 15) << 2;
+        final int var24 = (this.permutations[var16] & 15) << 2;
+        final int var25 = (this.permutations[var17] & 15) << 2;
+        final int var26 = (this.permutations[var18] & 15) << 2;
+        final int var27 = (this.permutations[var19] & 15) << 2;
         final double var60 = localX - 1.0;
         final double var61 = localY - 1.0;
         final double var62 = localZ - 1.0;
-        final double var87 = var36 * localX + var37 * localY + var38 * localZ;
-        final double var88 = var39 * var60 + var40 * localY + var41 * localZ;
-        final double var89 = var42 * localX + var43 * var61 + var44 * localZ;
-        final double var90 = var45 * var60 + var46 * var61 + var47 * localZ;
-        final double var91 = var48 * localX + var49 * localY + var50 * var62;
-        final double var92 = var51 * var60 + var52 * localY + var53 * var62;
-        final double var93 = var54 * localX + var55 * var61 + var56 * var62;
-        final double var94 = var57 * var60 + var58 * var61 + var59 * var62;
+        final double var87 = FLAT_SIMPLEX_GRAD[(var20) | 0] * localX + FLAT_SIMPLEX_GRAD[(var20) | 1] * localY + FLAT_SIMPLEX_GRAD[(var20) | 2] * localZ;
+        final double var88 = FLAT_SIMPLEX_GRAD[(var21) | 0] * var60 + FLAT_SIMPLEX_GRAD[(var21) | 1] * localY + FLAT_SIMPLEX_GRAD[(var21) | 2] * localZ;
+        final double var89 = FLAT_SIMPLEX_GRAD[(var22) | 0] * localX + FLAT_SIMPLEX_GRAD[(var22) | 1] * var61 + FLAT_SIMPLEX_GRAD[(var22) | 2] * localZ;
+        final double var90 = FLAT_SIMPLEX_GRAD[(var23) | 0] * var60 + FLAT_SIMPLEX_GRAD[(var23) | 1] * var61 + FLAT_SIMPLEX_GRAD[(var23) | 2] * localZ;
+        final double var91 = FLAT_SIMPLEX_GRAD[(var24) | 0] * localX + FLAT_SIMPLEX_GRAD[(var24) | 1] * localY + FLAT_SIMPLEX_GRAD[(var24) | 2] * var62;
+        final double var92 = FLAT_SIMPLEX_GRAD[(var25) | 0] * var60 + FLAT_SIMPLEX_GRAD[(var25) | 1] * localY + FLAT_SIMPLEX_GRAD[(var25) | 2] * var62;
+        final double var93 = FLAT_SIMPLEX_GRAD[(var26) | 0] * localX + FLAT_SIMPLEX_GRAD[(var26) | 1] * var61 + FLAT_SIMPLEX_GRAD[(var26) | 2] * var62;
+        final double var94 = FLAT_SIMPLEX_GRAD[(var27) | 0] * var60 + FLAT_SIMPLEX_GRAD[(var27) | 1] * var61 + FLAT_SIMPLEX_GRAD[(var27) | 2] * var62;
 
         final double var95 = localX * 6.0 - 15.0;
         final double var96 = fadeLocalX * 6.0 - 15.0;
--- src/main/java/com/ishland/c2me/mixin/optimization/math/MixinOctavePerlinNoiseSampler.java
+++ src/main/java/com/ishland/c2me/mixin/optimization/math/MixinOctavePerlinNoiseSampler.java
@@ -1,11 +1,39 @@
 package com.ishland.c2me.mixin.optimization.math;
 
+import it.unimi.dsi.fastutil.doubles.DoubleList;
 import net.minecraft.util.math.noise.OctavePerlinNoiseSampler;
+import net.minecraft.util.math.noise.PerlinNoiseSampler;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(OctavePerlinNoiseSampler.class)
 public class MixinOctavePerlinNoiseSampler {
+
+    @Shadow @Final private double lacunarity;
+
+    @Shadow @Final private double persistence;
+
+    @Shadow @Final private PerlinNoiseSampler[] octaveSamplers;
+
+    @Shadow @Final private DoubleList amplitudes;
+
+    @Unique
+    private int octaveSamplersCount = 0;
+
+    @Unique
+    private double[] amplitudesArray = null;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void onInit(CallbackInfo ci) {
+        this.octaveSamplersCount = this.octaveSamplers.length;
+        this.amplitudesArray = this.amplitudes.toDoubleArray();
+    }
 
     /**
      * @author ishland
@@ -14,6 +42,33 @@ public class MixinOctavePerlinNoiseSampler {
     @Overwrite
     public static double maintainPrecision(double value) {
         return value - Math.floor(value / 3.3554432E7 + 0.5) * 3.3554432E7;
+    }
+
+    /**
+     * @author ishland
+     * @reason optimize for common cases
+     */
+    @Overwrite
+    public double sample(double x, double y, double z) {
+        double d = 0.0;
+        double e = this.lacunarity;
+        double f = this.persistence;
+
+        for(int i = 0; i < this.octaveSamplersCount; ++i) {
+            PerlinNoiseSampler perlinNoiseSampler = this.octaveSamplers[i];
+            if (perlinNoiseSampler != null) {
+                @SuppressWarnings("deprecation")
+                double g = perlinNoiseSampler.sample(
+                        maintainPrecision(x * e), maintainPrecision(y * e), maintainPrecision(z * e), 0.0, 0.0
+                );
+                d += this.amplitudesArray[i] * g * f;
+            }
+
+            e *= 2.0;
+            f /= 2.0;
+        }
+
+        return d;
     }
 
 }
