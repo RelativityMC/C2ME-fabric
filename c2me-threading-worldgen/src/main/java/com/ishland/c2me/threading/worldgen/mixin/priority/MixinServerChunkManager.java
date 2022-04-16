050f1b2939eb4c332b5e3cfa1517e8628c7f7747
ishland <ishlandmc@yeah.net>
2022-04-16 21:25:47 +0800

    Only raise priority when it is really a sync load

--- c2me-threading-worldgen/src/main/java/com/ishland/c2me/threading/worldgen/mixin/priority/MixinServerChunkManager.java
+++ c2me-threading-worldgen/src/main/java/com/ishland/c2me/threading/worldgen/mixin/priority/MixinServerChunkManager.java
@@ -2,10 +2,13 @@ package com.ishland.c2me.threading.worldgen.mixin.priority;
 
 import com.ishland.c2me.base.common.scheduler.SchedulerThread;
 import com.ishland.c2me.threading.worldgen.common.ISyncLoadManager;
+import net.minecraft.server.world.ChunkHolder;
 import net.minecraft.server.world.ServerChunkManager;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkStatus;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,14 +18,28 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerChunkManager.class)
-public class MixinServerChunkManager implements ISyncLoadManager {
+public abstract class MixinServerChunkManager implements ISyncLoadManager {
 
-    @Shadow @Final private Thread serverThread;
+    @Shadow
+    @Final
+    private Thread serverThread;
+
+    @Shadow
+    protected abstract boolean isMissingForLevel(@Nullable ChunkHolder holder, int maxLevel);
+
+    @Shadow
+    @Nullable
+    protected abstract ChunkHolder getChunkHolder(long pos);
+
     @Unique
     private volatile ChunkPos currentSyncLoadChunk = null;
 
-    @Inject(method = "getChunk(IILnet/minecraft/world/chunk/ChunkStatus;Z)Lnet/minecraft/world/chunk/Chunk;", at = @At("HEAD"))
-    private void beforeGetChunk(int x, int z, ChunkStatus leastStatus, boolean create, CallbackInfoReturnable<Chunk> cir) {
+    @Dynamic
+    @Inject(method = {
+            "getChunk(IILnet/minecraft/world/chunk/ChunkStatus;Z)Lnet/minecraft/world/chunk/Chunk;",
+            "getChunkBlocking(IILnet/minecraft/world/chunk/ChunkStatus;Z)Lnet/minecraft/world/chunk/Chunk;"},
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerChunkManager$MainThreadExecutor;runTasks(Ljava/util/function/BooleanSupplier;)V", shift = At.Shift.BEFORE), require = 0)
+    private void beforeAwaitChunk(int x, int z, ChunkStatus leastStatus, boolean create, CallbackInfoReturnable<Chunk> cir) {
         if (Thread.currentThread() != this.serverThread) return;
 
         this.currentSyncLoadChunk = new ChunkPos(x, z);
@@ -33,8 +50,10 @@ public class MixinServerChunkManager implements ISyncLoadManager {
     private void afterGetChunk(int x, int z, ChunkStatus leastStatus, boolean create, CallbackInfoReturnable<Chunk> cir) {
         if (Thread.currentThread() != this.serverThread) return;
 
-        this.currentSyncLoadChunk = null;
-        SchedulerThread.INSTANCE.notifyPriorityChange();
+        if (this.currentSyncLoadChunk != null) {
+            this.currentSyncLoadChunk = null;
+            SchedulerThread.INSTANCE.notifyPriorityChange();
+        }
     }
 
     @Override
