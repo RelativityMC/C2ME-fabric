54e68bc8d2606d4b230cf819f17042fdaa98f004
ishland <ishlandmc@yeah.net>
2022-04-22 22:24:32 +0800

    Remove a classpath hack as fabric-loader already does this for us

--- c2me-fixes-worldgen-threading-issues/src/main/java/com/ishland/c2me/fixes/worldgen/threading_issues/ModuleEntryPoint.java
+++ c2me-fixes-worldgen-threading-issues/src/main/java/com/ishland/c2me/fixes/worldgen/threading_issues/ModuleEntryPoint.java
@@ -1,13 +1,7 @@
 package com.ishland.c2me.fixes.worldgen.threading_issues;
 
-import com.ishland.c2me.fixes.worldgen.threading_issues.common.DataFixerUpperClasspathFix;
-
 public class ModuleEntryPoint {
 
     private static final boolean enabled = true;
-
-    static {
-        DataFixerUpperClasspathFix.fix();
-    }
 
 }
