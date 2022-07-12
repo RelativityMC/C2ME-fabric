24c423f230aa80a01a90fbc283b9a112e0ba9790
ishland <ishlandmc@yeah.net>
2022-07-12 19:12:06 +0800

    microopt: use identity hashmap to avoid expensive hashCode()

---
+++ c2me-opts-worldgen-vanilla/src/main/java/com/ishland/c2me/opts/worldgen/vanilla/mixin/noise/MixinChunkNoiseSampler.java
@@ -0,0 +1,27 @@
+package com.ishland.c2me.opts.worldgen.vanilla.mixin.noise;
+
+import it.unimi.dsi.fastutil.objects.Reference2ReferenceOpenHashMap;
+import net.minecraft.world.gen.chunk.ChunkNoiseSampler;
+import net.minecraft.world.gen.densityfunction.DensityFunction;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
+
+@Mixin(ChunkNoiseSampler.class)
+public class MixinChunkNoiseSampler {
+
+    @Mutable
+    @Shadow @Final private Map<DensityFunction, DensityFunction> field_36582;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void onInit(CallbackInfo info) {
+        this.field_36582 = new Reference2ReferenceOpenHashMap<>(this.field_36582);
+    }
+
+}
