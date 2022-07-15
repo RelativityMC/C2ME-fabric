459b2b1782950f346ec3bd60165cbb5ff18ee9c0
ishland <ishlandmc@yeah.net>
2022-07-15 19:37:26 +0800

    Fix some config options

---
+++ c2me-opts-scheduling/src/main/java/com/ishland/c2me/opts/scheduling/mixin/MixinPlugin.java
@@ -0,0 +1,22 @@
+package com.ishland.c2me.opts.scheduling.mixin;
+
+import com.ishland.c2me.base.common.ModuleMixinPlugin;
+import com.ishland.c2me.opts.scheduling.common.Config;
+
+public class MixinPlugin extends ModuleMixinPlugin {
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (super.shouldApplyMixin(targetClassName, mixinClassName)) {
+            if (mixinClassName.startsWith("com.ishland.c2me.opts.scheduling.mixin.idle_tasks.autosave.disable_vanilla_mid_tick_autosave."))
+                return Config.autoSaveMode != Config.AutoSaveMode.VANILLA;
+            if (mixinClassName.startsWith("com.ishland.c2me.opts.scheduling.mixin.idle_tasks.autosave.enhanced_autosave."))
+                return Config.autoSaveMode == Config.AutoSaveMode.ENHANCED;
+            if (mixinClassName.startsWith("com.ishland.c2me.opts.scheduling.mixin.chunkscheduling.mid_tick_chunk_tasks."))
+                return Config.doMidTickChunkTasks;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
