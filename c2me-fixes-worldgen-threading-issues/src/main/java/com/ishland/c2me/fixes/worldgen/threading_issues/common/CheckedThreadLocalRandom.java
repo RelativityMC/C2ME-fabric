c1ad577c792993b284229ba1472d56860e2c66c7
ishland <ishlandmc@yeah.net>
2022-07-15 11:08:09 +0800

    Fix a crash

--- c2me-fixes-worldgen-threading-issues/src/main/java/com/ishland/c2me/fixes/worldgen/threading_issues/common/CheckedThreadLocalRandom.java
+++ c2me-fixes-worldgen-threading-issues/src/main/java/com/ishland/c2me/fixes/worldgen/threading_issues/common/CheckedThreadLocalRandom.java
@@ -3,6 +3,7 @@ package com.ishland.c2me.fixes.worldgen.threading_issues.common;
 import net.minecraft.util.math.random.LocalRandom;
 
 import java.util.ConcurrentModificationException;
+import java.util.Objects;
 import java.util.function.Supplier;
 
 public class CheckedThreadLocalRandom extends LocalRandom {
@@ -11,18 +12,20 @@ public class CheckedThreadLocalRandom extends LocalRandom {
 
     public CheckedThreadLocalRandom(long seed, Supplier<Thread> owner) {
         super(seed);
-        this.owner = owner;
+        this.owner = Objects.requireNonNull(owner);
     }
 
     @Override
     public void setSeed(long seed) {
-        if (Thread.currentThread() != this.owner.get()) throw new ConcurrentModificationException();
+        Thread owner = this.owner != null ? this.owner.get() : null;
+        if (owner != null && Thread.currentThread() != owner) throw new ConcurrentModificationException();
         super.setSeed(seed);
     }
 
     @Override
     public int next(int bits) {
-        if (Thread.currentThread() != this.owner.get()) throw new ConcurrentModificationException();
+        Thread owner = this.owner != null ? this.owner.get() : null;
+        if (owner != null && Thread.currentThread() != owner) throw new ConcurrentModificationException();
         return super.next(bits);
     }
 }
