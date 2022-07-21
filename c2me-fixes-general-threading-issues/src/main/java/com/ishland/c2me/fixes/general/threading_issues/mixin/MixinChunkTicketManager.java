f0658f717c2200faf285794cba3283dcdf12a311
ishland <ishlandmc@yeah.net>
2022-07-21 16:33:52 +0800

    Handle chunk holder iteration in a different way

---
+++ c2me-fixes-general-threading-issues/src/main/java/com/ishland/c2me/fixes/general/threading_issues/mixin/MixinChunkTicketManager.java
@@ -0,0 +1,23 @@
+package com.ishland.c2me.fixes.general.threading_issues.mixin;
+
+import net.minecraft.server.world.ChunkTicketManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Set;
+import java.util.function.Consumer;
+
+@Mixin(ChunkTicketManager.class)
+public class MixinChunkTicketManager {
+
+    @SuppressWarnings("unchecked")
+    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Ljava/util/Set;forEach(Ljava/util/function/Consumer;)V"))
+    private <T> void replaceIterationForHolderTicking(Set<T> instance, Consumer<T> consumer) {
+        if (instance.isEmpty()) return;
+        for (Object object : instance.toArray()) {
+            consumer.accept((T) object);
+        }
+    }
+
+}
