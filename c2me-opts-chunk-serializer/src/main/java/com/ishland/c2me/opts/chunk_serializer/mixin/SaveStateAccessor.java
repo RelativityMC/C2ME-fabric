a52a102bdef0bf5484b636052d7b701e9f14152a
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2022-08-07 22:49:43 +0200

    Fix compile issue

--- c2me-opts-chunk-serializer/src/main/java/com/ishland/c2me/opts/chunk_serializer/mixin/SaveStateAccessor.java
+++ c2me-opts-chunk-serializer/src/main/java/com/ishland/c2me/opts/chunk_serializer/mixin/SaveStateAccessor.java
@@ -1,13 +1,15 @@
 package com.ishland.c2me.opts.chunk_serializer.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(targets = "ca.spottedleaf.starlight.common.light.SWMRNibbleArray$SaveState")
+@Pseudo
+@Mixin(remap = false, targets = "ca.spottedleaf.starlight.common.light.SWMRNibbleArray$SaveState")
 public interface SaveStateAccessor {
-    @Accessor
+    @Accessor(remap = false)
     byte[] getData();
 
-    @Accessor
+    @Accessor(remap = false)
     int getState();
 }
