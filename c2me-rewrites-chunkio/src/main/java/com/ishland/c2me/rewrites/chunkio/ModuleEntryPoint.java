69677ab04e4aba509090014b72d93b40f621ac14
ishland <ishlandmc@yeah.net>
2023-01-03 23:49:20 +0800

    Enable ioSystem.replaceImpl only if worker parallelism >= 2

--- c2me-rewrites-chunkio/src/main/java/com/ishland/c2me/rewrites/chunkio/ModuleEntryPoint.java
+++ c2me-rewrites-chunkio/src/main/java/com/ishland/c2me/rewrites/chunkio/ModuleEntryPoint.java
@@ -1,13 +1,12 @@
 package com.ishland.c2me.rewrites.chunkio;
 
 import com.ishland.c2me.base.common.config.ConfigSystem;
-import io.netty.util.internal.PlatformDependent;
 
 public class ModuleEntryPoint {
 
     private static final boolean enabled = new ConfigSystem.ConfigAccessor()
             .key("ioSystem.replaceImpl")
             .comment("Whether to use the optimized implementation of IO system")
-            .getBoolean(!PlatformDependent.isWindows(), false);
+            .getBoolean(com.ishland.c2me.base.ModuleEntryPoint.globalExecutorParallelism >= 2, false);
 
 }
