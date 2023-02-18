3eb1512324a516f49052c6049a082a60d7bab14f
ishland <ishlandmc@yeah.net>
2023-02-19 00:06:24 +0800

    An attempt in making CheckedThreadLocalRandom message more clear
    Related to #197

--- c2me-fixes-worldgen-threading-issues/src/main/java/com/ishland/c2me/fixes/worldgen/threading_issues/ModuleEntryPoint.java
+++ c2me-fixes-worldgen-threading-issues/src/main/java/com/ishland/c2me/fixes/worldgen/threading_issues/ModuleEntryPoint.java
@@ -1,7 +1,13 @@
 package com.ishland.c2me.fixes.worldgen.threading_issues;
 
+import com.ishland.c2me.fixes.worldgen.threading_issues.common.Config;
+
 public class ModuleEntryPoint {
 
     private static final boolean enabled = true;
+
+    static {
+        Config.init();
+    }
 
 }
