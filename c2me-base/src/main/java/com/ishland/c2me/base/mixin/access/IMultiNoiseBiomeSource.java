c9913749e7991716cc22952ba770822765597948
ishland <ishlandmc@yeah.net>
2023-02-23 19:14:14 +0800

    1.19.4-pre1 and drops thread local biome cache

--- c2me-base/src/main/java/com/ishland/c2me/base/mixin/access/IMultiNoiseBiomeSource.java
+++ c2me-base/src/main/java/com/ishland/c2me/base/mixin/access/IMultiNoiseBiomeSource.java
@@ -1,15 +1,9 @@
 package com.ishland.c2me.base.mixin.access;
 
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.source.MultiNoiseBiomeSource;
-import net.minecraft.world.biome.source.util.MultiNoiseUtil;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(MultiNoiseBiomeSource.class)
 public interface IMultiNoiseBiomeSource {
-
-    @Accessor
-    MultiNoiseUtil.Entries<Biome> getBiomeEntries();
 
 }
