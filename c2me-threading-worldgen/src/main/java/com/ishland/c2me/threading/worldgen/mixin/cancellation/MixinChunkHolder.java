a4928b092ffcd4b7f8768a45894e9b882085c466
ishland <ishlandmc@yeah.net>
2023-03-04 22:23:44 +0800

    perf: better chunk cancellation mechanism
    Previous implementation of chunk cancellation is risk of raced double
    scheduling. This new mechanism implements chunk cancellation in a safer
    way to prevent this.

--- c2me-threading-worldgen/src/main/java/com/ishland/c2me/threading/worldgen/mixin/cancellation/MixinChunkHolder.java
+++ c2me-threading-worldgen/src/main/java/com/ishland/c2me/threading/worldgen/mixin/cancellation/MixinChunkHolder.java
@@ -2,17 +2,13 @@ package com.ishland.c2me.threading.worldgen.mixin.cancellation;
 
 import com.mojang.datafixers.util.Either;
 import net.minecraft.server.world.ChunkHolder;
-import net.minecraft.server.world.ThreadedAnvilChunkStorage;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.WorldChunk;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 import java.util.concurrent.atomic.AtomicReferenceArray;
 
 @Mixin(ChunkHolder.class)
@@ -26,14 +22,14 @@ public abstract class MixinChunkHolder {
 
     @Shadow @Final public static Either<Chunk, ChunkHolder.Unloaded> UNLOADED_CHUNK;
 
-    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Ljava/util/concurrent/atomic/AtomicReferenceArray;get(I)Ljava/lang/Object;"))
-    private <E> E captureWorldGenCancellation(AtomicReferenceArray<E> instance, int i, ThreadedAnvilChunkStorage chunkStorage, Executor executor) {
-        if (instance == this.futuresByStatus) {
-            final CompletableFuture<Either<Chunk, ChunkHolder.Unloaded>> future = this.futuresByStatus.get(i);
-            if (future == null) return null;
-            future.complete(UNLOADED_CHUNK);
-            return (E) future;
-        } else return instance.get(i);
-    }
+//    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Ljava/util/concurrent/atomic/AtomicReferenceArray;get(I)Ljava/lang/Object;"))
+//    private <E> E captureWorldGenCancellation(AtomicReferenceArray<E> instance, int i, ThreadedAnvilChunkStorage chunkStorage, Executor executor) {
+//        if (instance == this.futuresByStatus) {
+//            final CompletableFuture<Either<Chunk, ChunkHolder.Unloaded>> future = this.futuresByStatus.get(i);
+//            if (future == null) return null;
+//            future.complete(UNLOADED_CHUNK);
+//            return (E) future;
+//        } else return instance.get(i);
+//    }
 
 }
--- c2me-threading-worldgen/src/main/java/com/ishland/c2me/threading/worldgen/mixin/cancellation/MixinThreadedAnvilChunkStorage.java
+++ c2me-threading-worldgen/src/main/java/com/ishland/c2me/threading/worldgen/mixin/cancellation/MixinThreadedAnvilChunkStorage.java
@@ -1,13 +1,31 @@
 package com.ishland.c2me.threading.worldgen.mixin.cancellation;
 
+import com.mojang.datafixers.util.Either;
+import net.minecraft.server.world.ChunkHolder;
 import net.minecraft.server.world.ThreadedAnvilChunkStorage;
+import net.minecraft.util.thread.ThreadExecutor;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkStatus;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 
 @Mixin(ThreadedAnvilChunkStorage.class)
-public class MixinThreadedAnvilChunkStorage {
+public abstract class MixinThreadedAnvilChunkStorage {
+
+    @Shadow @Final private ThreadExecutor<Runnable> mainThreadExecutor;
+
+    @Shadow public abstract CompletableFuture<Either<Chunk, ChunkHolder.Unloaded>> getChunk(ChunkHolder holder, ChunkStatus requiredStatus);
+
+    @Shadow @Final private static Logger LOGGER;
 
     @Redirect(method = "getChunk", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/ChunkStatus;getDistanceFromFull(Lnet/minecraft/world/chunk/ChunkStatus;)I"))
     private int redirectAddLightTicketDistance(ChunkStatus status) {
@@ -17,6 +35,24 @@ public class MixinThreadedAnvilChunkStorage {
     @Redirect(method = "method_20443", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/ChunkStatus;getDistanceFromFull(Lnet/minecraft/world/chunk/ChunkStatus;)I"))
     private int redirectRemoveLightTicketDistance(ChunkStatus status) {
         return status == ChunkStatus.LIGHT ? ChunkStatus.getDistanceFromFull(ChunkStatus.STRUCTURE_STARTS) - 2 : ChunkStatus.getDistanceFromFull(status);
+    }
+
+    @Inject(method = "getChunk", at = @At("RETURN"), cancellable = true)
+    private void injectCancellationHook(ChunkHolder holder, ChunkStatus requiredStatus, CallbackInfoReturnable<CompletableFuture<Either<Chunk, ChunkHolder.Unloaded>>> cir) {
+        cir.setReturnValue(cir.getReturnValue().thenCompose(either -> {
+            if (either.right().isPresent()) {
+                return CompletableFuture.supplyAsync(() -> {
+                    if (ChunkHolder.getTargetStatusForLevel(holder.getLevel()).isAtLeast(requiredStatus)) {
+                        LOGGER.info("Chunk load {} raced, recovering", holder.getPos());
+                        return this.getChunk(holder, requiredStatus); // recover from cancellation
+                    } else {
+                        return CompletableFuture.completedFuture(either);
+                    }
+                }, this.mainThreadExecutor).thenCompose(Function.identity());
+            } else {
+                return CompletableFuture.completedFuture(either);
+            }
+        }));
     }
 
 }
