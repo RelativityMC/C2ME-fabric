edb5e17f3e9cd95bff0799cd271dd0bf9bb20de6
ishland <ishlandmc@yeah.net>
2023-05-27 23:19:05 +0800

    docs: remove redundant warning in config

--- c2me-rewrites-chunk-serializer/src/main/java/com/ishland/c2me/rewrites/chunk_serializer/ModuleEntryPoint.java
+++ c2me-rewrites-chunk-serializer/src/main/java/com/ishland/c2me/rewrites/chunk_serializer/ModuleEntryPoint.java
@@ -9,8 +9,6 @@ public final class ModuleEntryPoint {
             .key("ioSystem.gcFreeChunkSerializer")
             .comment("""
                     EXPERIMENTAL FEATURE, DO NOT USE UNLESS YOU KNOW WHAT YOU ARE DOING
-                    Do these before using this option
-                    - disable lithium mixin.world.tick_scheduler
                     
                     Whether to use the fast reduced allocation chunk serializer
                     (may cause incompatibility with other mods)
