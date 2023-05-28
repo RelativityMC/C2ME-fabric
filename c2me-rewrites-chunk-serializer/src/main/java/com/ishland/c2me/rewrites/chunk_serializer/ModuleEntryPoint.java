5c5b07890513825c312814b43a576f45d75c20dc
ishland <ishlandmc@yeah.net>
2023-05-28 14:25:59 +0800

    docs: make gcFreeChunkSerializer less scary [ci skip]

--- c2me-rewrites-chunk-serializer/src/main/java/com/ishland/c2me/rewrites/chunk_serializer/ModuleEntryPoint.java
+++ c2me-rewrites-chunk-serializer/src/main/java/com/ishland/c2me/rewrites/chunk_serializer/ModuleEntryPoint.java
@@ -8,7 +8,10 @@ public final class ModuleEntryPoint {
     public static final boolean enabled = new ConfigSystem.ConfigAccessor()
             .key("ioSystem.gcFreeChunkSerializer")
             .comment("""
-                    EXPERIMENTAL FEATURE, DO NOT USE UNLESS YOU KNOW WHAT YOU ARE DOING
+                    EXPERIMENTAL FEATURE
+                    This replaces the way your chunks are saved.
+                    Please keep regular backups of your world if you are using this feature,
+                    and report any world issues you encounter with this feature to our GitHub.
                     
                     Whether to use the fast reduced allocation chunk serializer
                     (may cause incompatibility with other mods)
