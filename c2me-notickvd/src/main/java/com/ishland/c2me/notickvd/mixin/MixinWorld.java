8befcb88114528f559d84d21da30502ff2654770
ishland <ishlandmc@yeah.net>
2023-07-13 14:24:21 +0800

    chore: clean up notickvd a bit

--- c2me-notickvd/src/main/java/com/ishland/c2me/notickvd/mixin/MixinWorld.java
+++ c2me-notickvd/src/main/java/com/ishland/c2me/notickvd/mixin/MixinWorld.java
@@ -1,29 +1,21 @@
 package com.ishland.c2me.notickvd.mixin;
 
-import net.minecraft.block.BlockState;
 import net.minecraft.server.world.ChunkLevelType;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.chunk.WorldChunk;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(World.class)
 public class MixinWorld {
 
     @Shadow @Final public boolean isClient;
 
-    @Inject(method = "setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;II)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ChunkLevelType;isAfter(Lnet/minecraft/server/world/ChunkLevelType;)Z"), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void redirectTickingStatus(BlockPos pos, BlockState state, int flags, int maxUpdateDepth, CallbackInfoReturnable<Boolean> cir, WorldChunk worldChunk) {
-        if (!this.isClient && worldChunk != null && worldChunk.getLevelType() == ChunkLevelType.FULL) {
-            ((ServerWorld) (Object) this).getChunkManager().markForUpdate(pos);
-        }
+    @ModifyArg(method = "setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;II)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ChunkLevelType;isAfter(Lnet/minecraft/server/world/ChunkLevelType;)Z"))
+    private ChunkLevelType modifyLeastStatus(ChunkLevelType levelType) {
+        return levelType.ordinal() > ChunkLevelType.FULL.ordinal() ? ChunkLevelType.FULL : levelType;
     }
 
 }
