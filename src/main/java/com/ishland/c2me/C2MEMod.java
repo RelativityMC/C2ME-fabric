6656e2d2636274e698be04ef22302ce49c9a0dde
AntonIXO <aivanovmailru@gmail.com>
2023-07-15 00:21:04 +0300

    Add benchmark

--- src/main/java/com/ishland/c2me/C2MEMod.java
+++ src/main/java/com/ishland/c2me/C2MEMod.java
@@ -7,6 +7,8 @@ import net.minecraft.util.math.random.LocalRandom;
 import net.minecraft.world.storage.ChunkStreamVersion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.github.luben.zstd.ZstdInputStream;
+import com.github.luben.zstd.ZstdOutputStream;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -28,14 +30,16 @@ public class C2MEMod implements ModInitializer {
         if (Boolean.getBoolean("com.ishland.c2me.runCompressionBenchmark")) {
             LOGGER.info("Benchmarking chunk stream speed");
             LOGGER.info("Warming up");
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < 4; i++) {
                 runBenchmark("GZIP", ChunkStreamVersion.GZIP, true);
                 runBenchmark("DEFLATE", ChunkStreamVersion.DEFLATE, true);
                 runBenchmark("UNCOMPRESSED", ChunkStreamVersion.UNCOMPRESSED, true);
+                runZstdBenchmark("zstd", true);
             }
             runBenchmark("GZIP", ChunkStreamVersion.GZIP, false);
             runBenchmark("DEFLATE", ChunkStreamVersion.DEFLATE, false);
             runBenchmark("UNCOMPRESSED", ChunkStreamVersion.UNCOMPRESSED, false);
+            runZstdBenchmark("zstd", false);
         }
         if (Boolean.getBoolean("com.ishland.c2me.runConsistencyTest")) {
             consistencyTest();
@@ -62,6 +66,37 @@ public class C2MEMod implements ModInitializer {
             {
                 final ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
                 final InputStream wrappedInputStream = version.wrap(inputStream);
+                long startTime = System.nanoTime();
+                final byte[] readAllBytes = wrappedInputStream.readAllBytes();
+                wrappedInputStream.close();
+                long endTime = System.nanoTime();
+                if (!suppressLog) LOGGER.info("{} read speed: {} MB/s ({} MB/s compressed)", name, decimalFormat.format((readAllBytes.length / 1024.0 / 1024.0) / ((endTime - startTime) / 1_000_000_000.0)), decimalFormat.format((outputStream.size() / 1024.0 / 1024.0) / ((endTime - startTime) / 1_000_000_000.0)));
+            }
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    private void runZstdBenchmark(String name, boolean suppressLog) {
+        try {
+            final DecimalFormat decimalFormat = new DecimalFormat("0.###");
+            if (!suppressLog) LOGGER.info("Generating 128MB random data");
+            final byte[] bytes = new byte[128 * 1024 * 1024];
+            new Random().nextBytes(bytes);
+            if (!suppressLog) LOGGER.info("Starting benchmark for zstd");
+            final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            {
+                final OutputStream wrappedOutputStream = new ZstdOutputStream(outputStream);
+                long startTime = System.nanoTime();
+                wrappedOutputStream.write(bytes);
+                wrappedOutputStream.close();
+                long endTime = System.nanoTime();
+                if (!suppressLog) LOGGER.info("{} write speed: {} MB/s ({} MB/s compressed)", name, decimalFormat.format((bytes.length / 1024.0 / 1024.0) / ((endTime - startTime) / 1_000_000_000.0)), decimalFormat.format((outputStream.size() / 1024.0 / 1024.0) / ((endTime - startTime) / 1_000_000_000.0)));
+                if (!suppressLog) LOGGER.info("{} compression ratio: {} %", name, decimalFormat.format(outputStream.size() / (double) bytes.length * 100.0));
+            }
+            {
+                final ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+                final InputStream wrappedInputStream = new ZstdInputStream(inputStream);
                 long startTime = System.nanoTime();
                 final byte[] readAllBytes = wrappedInputStream.readAllBytes();
                 wrappedInputStream.close();
