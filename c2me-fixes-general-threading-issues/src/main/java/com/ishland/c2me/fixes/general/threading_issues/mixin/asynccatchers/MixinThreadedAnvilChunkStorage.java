1ef727a9d677dd4d868226a69519723fbec60ba2
ishland <ishlandmc@yeah.net>
2024-02-07 20:06:07 +0800

    new: check for async entity loading and unloading

---
+++ c2me-fixes-general-threading-issues/src/main/java/com/ishland/c2me/fixes/general/threading_issues/mixin/asynccatchers/MixinThreadedAnvilChunkStorage.java
@@ -0,0 +1,33 @@
+package com.ishland.c2me.fixes.general.threading_issues.mixin.asynccatchers;
+
+import net.minecraft.server.world.ThreadedAnvilChunkStorage;
+import net.minecraft.util.thread.ThreadExecutor;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.ConcurrentModificationException;
+
+@Mixin(ThreadedAnvilChunkStorage.class)
+public class MixinThreadedAnvilChunkStorage {
+
+    @Shadow @Final private ThreadExecutor<Runnable> mainThreadExecutor;
+
+    @Inject(method = "loadEntity", at = @At("HEAD"))
+    private void preventAsyncEntityLoad(CallbackInfo ci) {
+        if (!this.mainThreadExecutor.isOnThread()) {
+            throw new ConcurrentModificationException("Async entity load");
+        }
+    }
+
+    @Inject(method = "unloadEntity", at = @At("HEAD"))
+    private void preventAsyncEntityUnload(CallbackInfo ci) {
+        if (!this.mainThreadExecutor.isOnThread()) {
+            throw new ConcurrentModificationException("Async entity unload");
+        }
+    }
+
+}
