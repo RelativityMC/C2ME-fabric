ea2569c3d2e937add72b79cc065855aaf0790d97
ishland <ishlandmc@yeah.net>
2024-04-22 18:19:36 +0800

    fix: guard ext_render_distance_v1 behind enabled checks

--- c2me-client-uncapvd/src/main/java/com/ishland/c2me/client/uncapvd/common/UncapVDInitializer.java
+++ c2me-client-uncapvd/src/main/java/com/ishland/c2me/client/uncapvd/common/UncapVDInitializer.java
@@ -1,13 +1,14 @@
 package com.ishland.c2me.client.uncapvd.common;
 
 import com.ishland.c2me.base.common.config.ModStatuses;
+import com.ishland.c2me.client.uncapvd.ModuleEntryPoint;
 import net.fabricmc.api.ClientModInitializer;
 
 public class UncapVDInitializer implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        if (ModStatuses.fabric_networking_api_v1) {
+        if (ModuleEntryPoint.enabled && ModStatuses.fabric_networking_api_v1) {
             ClientExtNetworking.registerListeners();
         }
     }
--- c2me-notickvd/src/main/java/com/ishland/c2me/notickvd/common/NoTickVDInitializer.java
+++ c2me-notickvd/src/main/java/com/ishland/c2me/notickvd/common/NoTickVDInitializer.java
@@ -1,12 +1,13 @@
 package com.ishland.c2me.notickvd.common;
 
 import com.ishland.c2me.base.common.config.ModStatuses;
+import com.ishland.c2me.notickvd.ModuleEntryPoint;
 import net.fabricmc.api.ModInitializer;
 
 public class NoTickVDInitializer implements ModInitializer {
     @Override
     public void onInitialize() {
-        if (Config.enableExtRenderDistanceProtocol && ModStatuses.fabric_networking_api_v1) {
+        if (ModuleEntryPoint.enabled && Config.enableExtRenderDistanceProtocol && ModStatuses.fabric_networking_api_v1) {
             ServerExtNetworking.registerListeners();
         }
     }
