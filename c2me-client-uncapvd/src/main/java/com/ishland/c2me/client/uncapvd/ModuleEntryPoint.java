a864fab4d55d73d20fbc9fe853533c08cf4dcab8
ishland <ishlandmc@yeah.net>
2024-04-22 18:19:36 +0800

    fix: guard ext_render_distance_v1 behind enabled checks

--- c2me-client-uncapvd/src/main/java/com/ishland/c2me/client/uncapvd/ModuleEntryPoint.java
+++ c2me-client-uncapvd/src/main/java/com/ishland/c2me/client/uncapvd/ModuleEntryPoint.java
@@ -5,7 +5,7 @@ import com.ishland.c2me.client.uncapvd.common.Config;
 
 public class ModuleEntryPoint {
 
-    private static final boolean enabled = new ConfigSystem.ConfigAccessor()
+    public static final boolean enabled = new ConfigSystem.ConfigAccessor()
             .key("clientSideConfig.modifyMaxVDConfig.enabled")
             .comment("Whether to modify maximum view distance")
             .incompatibleMod("bobby", "*")
