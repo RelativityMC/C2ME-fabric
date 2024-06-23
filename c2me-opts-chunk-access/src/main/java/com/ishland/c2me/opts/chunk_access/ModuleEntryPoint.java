e5247bd4f396b3cf29f022efacf781e740abb8a9
ishland <ishlandmc@yeah.net>
2024-06-23 19:42:05 +0800

    change: disable optimizeAsyncChunkRequest on 1.21 by default

--- c2me-opts-chunk-access/src/main/java/com/ishland/c2me/opts/chunk_access/ModuleEntryPoint.java
+++ c2me-opts-chunk-access/src/main/java/com/ishland/c2me/opts/chunk_access/ModuleEntryPoint.java
@@ -7,7 +7,7 @@ public class ModuleEntryPoint {
     public static final boolean enabled = new ConfigSystem.ConfigAccessor()
             .key("generalOptimizations.optimizeAsyncChunkRequest")
             .comment("Whether to let async chunk request no longer block server thread\n" +
-                    "(may cause incompatibility with other mods)")
-            .getBoolean(true, false);
+                    "Currently causes some unexplainable issues on 1.21, use with caution")
+            .getBoolean(false, false);
 
 }
