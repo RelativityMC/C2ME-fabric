79237adca45a7fd2bbdc46db0c2a6b256a6250ae
ishland <ishlandmc@yeah.net>
2024-06-24 16:59:03 +0800

    change: enable threaded worldgen based on configured parallelism

--- c2me-threading-worldgen/src/main/java/com/ishland/c2me/threading/worldgen/ModuleEntryPoint.java
+++ c2me-threading-worldgen/src/main/java/com/ishland/c2me/threading/worldgen/ModuleEntryPoint.java
@@ -3,14 +3,14 @@ package com.ishland.c2me.threading.worldgen;
 import com.ishland.c2me.base.common.config.ConfigSystem;
 import com.ishland.c2me.threading.worldgen.common.Config;
 
-import static com.ishland.c2me.base.ModuleEntryPoint.defaultParallelism;
+import static com.ishland.c2me.base.ModuleEntryPoint.globalExecutorParallelism;
 
 class ModuleEntryPoint {
 
     public static final boolean enabled = new ConfigSystem.ConfigAccessor()
             .key("threadedWorldGen.enabled")
             .comment("Whether to enable this feature")
-            .getBoolean(defaultParallelism >= 3, false);
+            .getBoolean(globalExecutorParallelism >= 3, false);
 
     static {
         Config.init();
