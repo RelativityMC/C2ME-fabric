d939bb37676a9dafa148721e27103675827179d4
ishland <ishlandmc@yeah.net>
2024-07-14 19:15:40 +0800

    change: disable replaceImpl as asyncio isn't integrated yet

--- c2me-rewrites-chunkio/src/main/java/com/ishland/c2me/rewrites/chunkio/ModuleEntryPoint.java
+++ c2me-rewrites-chunkio/src/main/java/com/ishland/c2me/rewrites/chunkio/ModuleEntryPoint.java
@@ -7,6 +7,6 @@ public class ModuleEntryPoint {
     private static final boolean enabled = new ConfigSystem.ConfigAccessor()
             .key("ioSystem.replaceImpl")
             .comment("Whether to use the optimized implementation of IO system")
-            .getBoolean(com.ishland.c2me.base.ModuleEntryPoint.globalExecutorParallelism >= 2, false);
+            .getBoolean(false, false);
 
 }
