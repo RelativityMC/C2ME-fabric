ab1e42650ceb2c023733d98b495727e93797c30b
ishland <ishlandmc@yeah.net>
2024-07-22 17:16:22 +0800

    fix: porting_lib compatibility regression
    Fixes #340

--- c2me-notickvd/src/main/java/com/ishland/c2me/notickvd/mixin/MixinWorld.java
+++ c2me-notickvd/src/main/java/com/ishland/c2me/notickvd/mixin/MixinWorld.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(World.class)
+@Mixin(value = World.class, priority = 1101)
 public class MixinWorld {
 
     @Shadow @Final public boolean isClient;
