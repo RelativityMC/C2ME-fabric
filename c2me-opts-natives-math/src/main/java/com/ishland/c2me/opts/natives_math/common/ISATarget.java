b8d0064f57badd65bec1fcb4f065a190517e67ec
ishland <ishlandmc@yeah.net>
2024-08-26 13:43:03 +0800

    refactor: avoid netty calls in benchmarks

--- c2me-opts-natives-math/src/main/java/com/ishland/c2me/opts/natives_math/common/ISATarget.java
+++ c2me-opts-natives-math/src/main/java/com/ishland/c2me/opts/natives_math/common/ISATarget.java
@@ -2,7 +2,6 @@ package com.ishland.c2me.opts.natives_math.common;
 
 import com.ishland.c2me.opts.natives_math.common.isa.ISA_aarch64;
 import com.ishland.c2me.opts.natives_math.common.isa.ISA_x86_64;
-import io.netty.util.internal.PlatformDependent;
 
 public interface ISATarget {
 
@@ -13,7 +12,7 @@ public interface ISATarget {
     boolean isNativelySupported();
 
     static Class<? extends Enum<? extends ISATarget>> getInstance() {
-        return switch (PlatformDependent.normalizedArch()) {
+        return switch (NativeLoader.NORMALIZED_ARCH) {
             case "x86_64" -> ISA_x86_64.class;
             case "aarch_64" -> ISA_aarch64.class;
             default -> null;
--- c2me-opts-natives-math/src/main/java/com/ishland/c2me/opts/natives_math/common/NativeLoader.java
+++ c2me-opts-natives-math/src/main/java/com/ishland/c2me/opts/natives_math/common/NativeLoader.java
@@ -1,6 +1,6 @@
 package com.ishland.c2me.opts.natives_math.common;
 
-import io.netty.util.internal.PlatformDependent;
+import io.netty.util.internal.SystemPropertyUtil;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,8 +8,12 @@ import java.lang.foreign.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.Locale;
 
 public class NativeLoader {
+
+    public static final String NORMALIZED_ARCH = normalizeArch(SystemPropertyUtil.get("os.arch", ""));
+    public static final String NORMALIZED_OS = normalizeOs(SystemPropertyUtil.get("os.name", ""));
 
     private static final Arena arena = Arena.ofAuto();
     public static final SymbolLookup lookup;
@@ -17,7 +21,7 @@ public class NativeLoader {
     public static final ISATarget currentMachineTarget;
 
     static {
-        String libName = String.format("%s-%s-%s", PlatformDependent.normalizedOs(), PlatformDependent.normalizedArch(), System.mapLibraryName("c2me-opts-natives-math"));
+        String libName = String.format("%s-%s-%s", NORMALIZED_OS, NORMALIZED_ARCH, System.mapLibraryName("c2me-opts-natives-math"));
         lookup = load0(libName);
         if (lookup == null) {
             currentMachineTarget = null;
@@ -56,6 +60,94 @@ public class NativeLoader {
             e.printStackTrace();
             return null;
         }
+    }
+
+    private static String normalize(String value) {
+        return value.toLowerCase(Locale.US).replaceAll("[^a-z0-9]+", "");
+    }
+
+    private static String normalizeArch(String value) {
+        value = normalize(value);
+        if (value.matches("^(x8664|amd64|ia32e|em64t|x64)$")) {
+            return "x86_64";
+        }
+        if (value.matches("^(x8632|x86|i[3-6]86|ia32|x32)$")) {
+            return "x86_32";
+        }
+        if (value.matches("^(ia64|itanium64)$")) {
+            return "itanium_64";
+        }
+        if (value.matches("^(sparc|sparc32)$")) {
+            return "sparc_32";
+        }
+        if (value.matches("^(sparcv9|sparc64)$")) {
+            return "sparc_64";
+        }
+        if (value.matches("^(arm|arm32)$")) {
+            return "arm_32";
+        }
+        if ("aarch64".equals(value)) {
+            return "aarch_64";
+        }
+        if (value.matches("^(ppc|ppc32)$")) {
+            return "ppc_32";
+        }
+        if ("ppc64".equals(value)) {
+            return "ppc_64";
+        }
+        if ("ppc64le".equals(value)) {
+            return "ppcle_64";
+        }
+        if ("s390".equals(value)) {
+            return "s390_32";
+        }
+        if ("s390x".equals(value)) {
+            return "s390_64";
+        }
+        if ("loongarch64".equals(value)) {
+            return "loongarch_64";
+        }
+
+        return "unknown";
+    }
+
+    private static String normalizeOs(String value) {
+        value = normalize(value);
+        if (value.startsWith("aix")) {
+            return "aix";
+        }
+        if (value.startsWith("hpux")) {
+            return "hpux";
+        }
+        if (value.startsWith("os400")) {
+            // Avoid the names such as os4000
+            if (value.length() <= 5 || !Character.isDigit(value.charAt(5))) {
+                return "os400";
+            }
+        }
+        if (value.startsWith("linux")) {
+            return "linux";
+        }
+        if (value.startsWith("macosx") || value.startsWith("osx") || value.startsWith("darwin")) {
+            return "osx";
+        }
+        if (value.startsWith("freebsd")) {
+            return "freebsd";
+        }
+        if (value.startsWith("openbsd")) {
+            return "openbsd";
+        }
+        if (value.startsWith("netbsd")) {
+            return "netbsd";
+        }
+        if (value.startsWith("solaris") || value.startsWith("sunos")) {
+            return "sunos";
+        }
+        if (value.startsWith("windows")) {
+            return "windows";
+        }
+
+        return "unknown";
     }
 
 }
