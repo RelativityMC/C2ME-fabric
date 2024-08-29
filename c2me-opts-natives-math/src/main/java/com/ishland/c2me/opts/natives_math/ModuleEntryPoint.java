daa466242727bdf40f6a74da76ae116bdd3b54fb
ishland <ishlandmc@yeah.net>
2024-08-29 18:40:55 +0800

    change: disable nativeaccel by default

--- c2me-opts-natives-math/src/main/java/com/ishland/c2me/opts/natives_math/ModuleEntryPoint.java
+++ c2me-opts-natives-math/src/main/java/com/ishland/c2me/opts/natives_math/ModuleEntryPoint.java
@@ -5,16 +5,12 @@ import com.ishland.c2me.opts.natives_math.common.NativeLoader;
 
 public class ModuleEntryPoint {
 
-    public static final boolean AVAILABLE = NativeLoader.lookup != null;
     private static final boolean enabled = new ConfigSystem.ConfigAccessor()
             .key("vanillaWorldGenOptimizations.nativeAcceleration")
             .comment("""
                     Enable the use of bundled native libraries to accelerate world generation
                     Availability on your system in the last game launch: %s
-                    
-                    The implementation currently have slight relative error from the vanilla implementation
-                    (generally within a few ulps)
                     """.formatted(NativeLoader.getAvailabilityString()))
-            .getBoolean(true, false) && AVAILABLE;
+            .getBoolean(false, false) && NativeLoader.lookup != null;
 
 }
