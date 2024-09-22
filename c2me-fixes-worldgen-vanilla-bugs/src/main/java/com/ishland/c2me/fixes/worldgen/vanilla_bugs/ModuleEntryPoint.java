3eb1b5e9654dfc6e2b24f0af4fafaf4ba2e6ddb3
ishland <ishlandmc@yeah.net>
2024-09-22 10:37:23 +0800

    Revert "fix: ghost mushroom attempt 3"
    This reverts commit ebd3b85d77bebecda82e33253a45877f4cc88e4f.

--- c2me-fixes-worldgen-vanilla-bugs/src/main/java/com/ishland/c2me/fixes/worldgen/vanilla_bugs/ModuleEntryPoint.java
+++ c2me-fixes-worldgen-vanilla-bugs/src/main/java/com/ishland/c2me/fixes/worldgen/vanilla_bugs/ModuleEntryPoint.java
@@ -1,13 +1,7 @@
 package com.ishland.c2me.fixes.worldgen.vanilla_bugs;
 
-import com.ishland.c2me.fixes.worldgen.vanilla_bugs.common.Config;
-
 public class ModuleEntryPoint {
 
     private static final boolean enabled = true;
-
-    static {
-        Config.init();
-    }
 
 }
