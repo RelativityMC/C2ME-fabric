54a200ceda08b400ecfd7bf2c5929b3620d76ec7
ishland <ishlandmc@yeah.net>
2025-02-10 23:38:34 +0800

    fix: block non-block-ticking chunks from ticking block entities
    Cherry-picked from 01805e7d19dd70eb336153da4071cc40017b8d10

--- c2me-rewrites-chunk-system/src/main/java/com/ishland/c2me/rewrites/chunksystem/mixin/MixinWorldChunk.java
+++ c2me-rewrites-chunk-system/src/main/java/com/ishland/c2me/rewrites/chunksystem/mixin/MixinWorldChunk.java
@@ -1,5 +1,7 @@
 package com.ishland.c2me.rewrites.chunksystem.mixin;
 
+import com.ishland.c2me.rewrites.chunksystem.common.ducks.WorldChunkExtension;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.block.entity.BlockEntity;
@@ -18,12 +20,16 @@ import net.minecraft.world.chunk.WorldChunk;
 import net.minecraft.world.gen.chunk.BlendingData;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.List;
 
 @Mixin(WorldChunk.class)
-public abstract class MixinWorldChunk  extends Chunk {
+public abstract class MixinWorldChunk extends Chunk implements WorldChunkExtension {
+
+    @Unique
+    private boolean c2me$blockTicking;
 
     public MixinWorldChunk(ChunkPos pos, UpgradeData upgradeData, HeightLimitView heightLimitView, Registry<Biome> biomeRegistry, long inhabitedTime, @Nullable ChunkSection[] sectionArray, @Nullable BlendingData blendingData) {
         super(pos, upgradeData, heightLimitView, biomeRegistry, inhabitedTime, sectionArray, blendingData);
@@ -41,6 +47,21 @@ public abstract class MixinWorldChunk  extends Chunk {
         }
 
         return blockEntity;
+    }
+
+    @Override
+    public boolean c2me$isBlockTicking() {
+        return this.c2me$blockTicking;
+    }
+
+    @Override
+    public void c2me$setBlockTicking(boolean blockTicking) {
+        this.c2me$blockTicking = blockTicking;
+    }
+
+    @WrapMethod(method = "canTickBlockEntity")
+    private boolean wrapCanTickBlockEntity(BlockPos pos, Operation<Boolean> original) {
+        return this.c2me$isBlockTicking() && original.call(pos);
     }
 
 }
