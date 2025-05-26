7684ab5da8672a699f05b0b668c1c806f24da457
ishland <ishlandmc@yeah.net>
2025-05-26 11:01:40 +0800

    new: add elapsed time to task dump

--- c2me-base/src/main/java/com/ishland/c2me/base/common/threadstate/SyncLoadWork.java
+++ c2me-base/src/main/java/com/ishland/c2me/base/common/threadstate/SyncLoadWork.java
@@ -1,14 +1,20 @@
 package com.ishland.c2me.base.common.threadstate;
 
+import com.ishland.c2me.base.common.util.TimeUtil;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.chunk.ChunkStatus;
 
-public record SyncLoadWork(ServerWorld world, ChunkPos chunkPos, ChunkStatus targetStatus, boolean create) implements RunningWork {
+public record SyncLoadWork(ServerWorld world, ChunkPos chunkPos, ChunkStatus targetStatus, boolean create, long startTime) implements RunningWork {
+
+    public SyncLoadWork(ServerWorld world, ChunkPos chunkPos, ChunkStatus targetStatus, boolean create) {
+        this(world, chunkPos, targetStatus, create, System.nanoTime());
+    }
 
     @Override
     public String toString() {
-        return String.format("Sync load chunk %s to status %s in world %s (create=%s)", chunkPos, targetStatus, world.getRegistryKey().getValue(), create);
+        return String.format("Sync load chunk %s to status %s in world %s (create=%s) (%s elapsed)",
+                chunkPos, targetStatus, world.getRegistryKey().getValue(), create, TimeUtil.formatElapsedTime(System.nanoTime() - startTime));
     }
 
 }
--- c2me-rewrites-chunk-system/src/main/java/com/ishland/c2me/rewrites/chunksystem/common/threadstate/ChunkTaskWork.java
+++ c2me-rewrites-chunk-system/src/main/java/com/ishland/c2me/rewrites/chunksystem/common/threadstate/ChunkTaskWork.java
@@ -1,20 +1,22 @@
 package com.ishland.c2me.rewrites.chunksystem.common.threadstate;
 
 import com.ishland.c2me.base.common.threadstate.RunningWork;
+import com.ishland.c2me.base.common.util.TimeUtil;
 import com.ishland.c2me.base.mixin.access.IThreadedAnvilChunkStorage;
 import com.ishland.c2me.rewrites.chunksystem.common.ChunkLoadingContext;
 import com.ishland.c2me.rewrites.chunksystem.common.NewChunkStatus;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.ChunkPos;
 
-public record ChunkTaskWork(ServerWorld world, ChunkPos chunkPos, NewChunkStatus status, boolean isUpgrade) implements RunningWork {
+public record ChunkTaskWork(ServerWorld world, ChunkPos chunkPos, NewChunkStatus status, boolean isUpgrade, long startTime) implements RunningWork {
 
     public ChunkTaskWork(ChunkLoadingContext context, NewChunkStatus status, boolean isUpgrade) {
         this(
                 ((IThreadedAnvilChunkStorage) context.tacs()).getWorld(),
                 context.holder().getKey(),
                 status,
-                isUpgrade
+                isUpgrade,
+                System.nanoTime()
         );
     }
 
@@ -22,17 +24,19 @@ public record ChunkTaskWork(ServerWorld world, ChunkPos chunkPos, NewChunkStatus
     public String toString() {
         if (isUpgrade) {
             return String.format(
-                    "Upgrading chunk %s to %s in world %s",
+                    "Upgrading chunk %s to %s in world %s (%s elapsed)",
                     chunkPos,
                     status,
-                    world.getRegistryKey().getValue()
+                    world.getRegistryKey().getValue(),
+                    TimeUtil.formatElapsedTime(System.nanoTime() - startTime)
             );
         } else {
             return String.format(
-                    "Downgrading chunk %s from %s in world %s",
+                    "Downgrading chunk %s from %s in world %s (%s elapsed)",
                     chunkPos,
                     status,
-                    world.getRegistryKey().getValue()
+                    world.getRegistryKey().getValue(),
+                    TimeUtil.formatElapsedTime(System.nanoTime() - startTime)
             );
         }
     }
