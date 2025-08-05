53e35b7cc24e26723e2d3e1c12e28e2aa7e6e241
ishland <ishlandmc@yeah.net>
2025-08-05 22:29:28 +0800

    build: 25w32a

--- c2me-notickvd/src/main/java/com/ishland/c2me/notickvd/mixin/client/MixinIntegratedServer.java
+++ c2me-notickvd/src/main/java/com/ishland/c2me/notickvd/mixin/client/MixinIntegratedServer.java
@@ -1,13 +1,13 @@
 package com.ishland.c2me.notickvd.mixin.client;
 
 import com.mojang.datafixers.DataFixer;
-import net.minecraft.class_11545;
 import net.minecraft.resource.ResourcePackManager;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.SaveLoader;
 import net.minecraft.server.integrated.IntegratedServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ApiServices;
+import net.minecraft.world.chunk.ChunkLoadProgress;
 import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,8 +19,8 @@ import java.net.Proxy;
 @Mixin(IntegratedServer.class)
 public abstract class MixinIntegratedServer extends MinecraftServer {
 
-    public MixinIntegratedServer(Thread serverThread, LevelStorage.Session session, ResourcePackManager dataPackManager, SaveLoader saveLoader, Proxy proxy, DataFixer dataFixer, ApiServices apiServices, class_11545 worldGenerationProgressListenerFactory) {
-        super(serverThread, session, dataPackManager, saveLoader, proxy, dataFixer, apiServices, worldGenerationProgressListenerFactory);
+    public MixinIntegratedServer(Thread serverThread, LevelStorage.Session session, ResourcePackManager dataPackManager, SaveLoader saveLoader, Proxy proxy, DataFixer dataFixer, ApiServices apiServices, ChunkLoadProgress chunkLoadProgress) {
+        super(serverThread, session, dataPackManager, saveLoader, proxy, dataFixer, apiServices, chunkLoadProgress);
     }
 
     @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/integrated/IntegratedServer;incrementTotalWorldTimeStat()V", shift = At.Shift.AFTER))
