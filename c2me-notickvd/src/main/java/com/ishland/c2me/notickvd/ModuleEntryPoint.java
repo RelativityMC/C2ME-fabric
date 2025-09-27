6dcf2fdf4217f298d0b4986f1c4919d4c6fa0b6a
ishland <ishlandmc@yeah.net>
2025-09-27 22:14:05 +0800

    change: enable notickvd always

--- c2me-notickvd/src/main/java/com/ishland/c2me/notickvd/ModuleEntryPoint.java
+++ c2me-notickvd/src/main/java/com/ishland/c2me/notickvd/ModuleEntryPoint.java
@@ -5,10 +5,7 @@ import com.ishland.c2me.notickvd.common.Config;
 
 public class ModuleEntryPoint {
 
-    public static final boolean enabled = new ConfigSystem.ConfigAccessor()
-            .key("noTickViewDistance.enabled")
-            .comment("Whether to enable no-tick view distance")
-            .getBoolean(true, false);
+    public static final boolean enabled = !Boolean.getBoolean("com.ishland.c2me.notickvd.disable");
 
     static {
         Config.init();
