8ae4dcb84c436325daf141d3817e7c53fa0b4183
ishland <ishlandmc@yeah.net>
2025-10-31 00:00:13 +0800

    fix: defer all level events to avoid clearing deferred state
    Cherry-picked from 46304928a46de37e3fafd39214d0b0c6bcf5ad1c

---
+++ c2me-rewrites-chunk-system/src/main/java/com/ishland/c2me/rewrites/chunksystem/common/ducks/TicketDistanceLevelPropagatorExtension.java
@@ -0,0 +1,9 @@
+package com.ishland.c2me.rewrites.chunksystem.common.ducks;
+
+import it.unimi.dsi.fastutil.longs.Long2IntLinkedOpenHashMap;
+
+public interface TicketDistanceLevelPropagatorExtension {
+
+    Long2IntLinkedOpenHashMap c2me$getTicketLevelUpdates();
+
+}
--- c2me-rewrites-chunk-system/src/main/java/com/ishland/c2me/rewrites/chunksystem/mixin/MixinChunkLevelManager.java
+++ c2me-rewrites-chunk-system/src/main/java/com/ishland/c2me/rewrites/chunksystem/mixin/MixinChunkLevelManager.java
@@ -2,22 +2,38 @@ package com.ishland.c2me.rewrites.chunksystem.mixin;
 
 import com.bawnorton.mixinsquared.TargetHandler;
 import com.ishland.c2me.rewrites.chunksystem.common.Config;
+import com.ishland.c2me.rewrites.chunksystem.common.ducks.TicketDistanceLevelPropagatorExtension;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import it.unimi.dsi.fastutil.longs.Long2IntLinkedOpenHashMap;
 import net.minecraft.server.world.ChunkHolder;
 import net.minecraft.server.world.ChunkLevelManager;
+import net.minecraft.server.world.ServerChunkLoadingManager;
 import net.minecraft.server.world.ThrottledChunkTaskScheduler;
+import net.minecraft.server.world.TicketDistanceLevelPropagator;
 import net.minecraft.util.thread.TaskExecutor;
+import org.jetbrains.annotations.Nullable;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Dynamic;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.concurrent.Executor;
 
 @Mixin(value = ChunkLevelManager.class, priority = 1051)
-public class MixinChunkLevelManager {
+public abstract class MixinChunkLevelManager {
+
+    @Shadow
+    @Final
+    private TicketDistanceLevelPropagator ticketDistanceLevelPropagator;
+
+    @Shadow
+    protected abstract @Nullable ChunkHolder setLevel(long pos, int level, @Nullable ChunkHolder holder, int i);
 
     @Dynamic
     @TargetHandler(
@@ -55,6 +71,18 @@ public class MixinChunkLevelManager {
             return original.call(executor, (Executor) Runnable::run, maxConcurrentChunks); // improve player ticket consistency
         } else {
             return original.call(executor, dispatchExecutor, maxConcurrentChunks);
+        }
+    }
+
+    @Inject(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/TicketDistanceLevelPropagator;update(I)I", shift = At.Shift.AFTER))
+    private void postTicketPropagator(ServerChunkLoadingManager chunkLoadingManager, CallbackInfoReturnable<Boolean> cir) {
+        if (this.ticketDistanceLevelPropagator != null) { // ignore if replaced
+            Long2IntLinkedOpenHashMap updates = ((TicketDistanceLevelPropagatorExtension) this.ticketDistanceLevelPropagator).c2me$getTicketLevelUpdates();
+            while (!updates.isEmpty()) {
+                long pos = updates.firstLongKey();
+                int level = updates.removeFirstInt();
+                this.setLevel(pos, level, null, Integer.MAX_VALUE - 1); // holder and old level is ignored
+            }
         }
     }
 
