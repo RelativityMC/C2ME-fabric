98bceb6b5c8f2be04b34d8d153dc27133d6ac592
ishland <ishlandmc@yeah.net>
2026-02-11 23:05:04 +0800

    fix: client
    Fixes #522

--- c2me-client-uncapvd/src/main/java/com/ishland/c2me/client/uncapvd/mixin/rendering/MixinFogRenderer.java
+++ c2me-client-uncapvd/src/main/java/com/ishland/c2me/client/uncapvd/mixin/rendering/MixinFogRenderer.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Mixin(FogRenderer.class)
 public class MixinFogRenderer {
 
-    @ModifyArg(method = "applyFog(Lnet/minecraft/client/render/Camera;ILnet/minecraft/client/render/RenderTickCounter;FLnet/minecraft/client/world/ClientWorld;)Lorg/joml/Vector4f;", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/fog/FogRenderer;getFogColor(Lnet/minecraft/client/render/Camera;FLnet/minecraft/client/world/ClientWorld;IF)Lorg/joml/Vector4f;"), index = 3)
+    @ModifyArg(method = "applyFog(Lnet/minecraft/client/render/Camera;ILnet/minecraft/client/render/RenderTickCounter;FLnet/minecraft/client/world/ClientWorld;)Lnet/minecraft/client/render/fog/FogData;", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/fog/FogRenderer;getFogColor(Lnet/minecraft/client/render/Camera;FLnet/minecraft/client/world/ClientWorld;IFLorg/joml/Vector4f;)V"), index = 3)
     private int overrideViewDistance(int viewDistance) {
         return Math.clamp(viewDistance, 2, 32);
     }
