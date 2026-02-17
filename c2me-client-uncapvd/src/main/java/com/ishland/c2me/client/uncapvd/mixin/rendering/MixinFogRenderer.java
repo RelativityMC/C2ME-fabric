52827465b9c13bcb3c58765f2b9b9e2eb29f4108
ishland <ishlandmc@yeah.net>
2026-02-18 00:33:09 +0800

    build: 26.1-snapshot-8

--- c2me-client-uncapvd/src/main/java/com/ishland/c2me/client/uncapvd/mixin/rendering/MixinFogRenderer.java
+++ c2me-client-uncapvd/src/main/java/com/ishland/c2me/client/uncapvd/mixin/rendering/MixinFogRenderer.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Mixin(FogRenderer.class)
 public class MixinFogRenderer {
 
-    @ModifyArg(method = "applyFog(Lnet/minecraft/client/render/Camera;ILnet/minecraft/client/render/RenderTickCounter;FLnet/minecraft/client/world/ClientWorld;)Lnet/minecraft/client/render/fog/FogData;", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/fog/FogRenderer;getFogColor(Lnet/minecraft/client/render/Camera;FLnet/minecraft/client/world/ClientWorld;IFLorg/joml/Vector4f;)V"), index = 3)
+    @ModifyArg(method = "prepareFog", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/fog/FogRenderer;getFogColor(Lnet/minecraft/client/render/Camera;FLnet/minecraft/client/world/ClientWorld;IFLorg/joml/Vector4f;)V"), index = 3)
     private int overrideViewDistance(int viewDistance) {
         return Math.clamp(viewDistance, 2, 32);
     }
