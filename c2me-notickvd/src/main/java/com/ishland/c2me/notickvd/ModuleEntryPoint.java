3397ac4e4d20cf90aff0a975412d445eca879f93
ishland <ishlandmc@yeah.net>
2026-02-28 17:05:26 +0800

    change: allow opt-out notickvd

--- c2me-notickvd/src/main/java/com/ishland/c2me/notickvd/ModuleEntryPoint.java
+++ c2me-notickvd/src/main/java/com/ishland/c2me/notickvd/ModuleEntryPoint.java
@@ -5,7 +5,14 @@ import com.ishland.c2me.notickvd.common.Config;
 
 public class ModuleEntryPoint {
 
-    public static final boolean enabled = !Boolean.getBoolean("com.ishland.c2me.notickvd.disable");
+    public static final boolean enabled = new ConfigSystem.ConfigAccessor()
+            .key("noTickViewDistance.enabled")
+            .comment("""
+                    Whether to enable no-tick view distance
+                    
+                    Faster alternative implementation of simulation distance, and additionally support up to 65530 view distance
+                    """)
+            .getBoolean(true, false);
 
     static {
         Config.init();
