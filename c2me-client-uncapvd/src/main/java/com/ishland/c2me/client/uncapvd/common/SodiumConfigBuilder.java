21c786407448704eccbcdf0093f92bb75d5e1f10
ishland <ishlandmc@yeah.net>
2026-03-27 14:19:56 +0800

    fix: don't register sodium rd override when disabled

--- c2me-client-uncapvd/src/main/java/com/ishland/c2me/client/uncapvd/common/SodiumConfigBuilder.java
+++ c2me-client-uncapvd/src/main/java/com/ishland/c2me/client/uncapvd/common/SodiumConfigBuilder.java
@@ -1,5 +1,6 @@
 package com.ishland.c2me.client.uncapvd.common;
 
+import com.ishland.c2me.client.uncapvd.ModuleEntryPoint;
 import net.caffeinemc.mods.sodium.api.config.ConfigEntryPoint;
 import net.caffeinemc.mods.sodium.api.config.structure.ConfigBuilder;
 import net.minecraft.util.Identifier;
@@ -7,6 +8,10 @@ import net.minecraft.util.Identifier;
 public class SodiumConfigBuilder implements ConfigEntryPoint {
     @Override
     public void registerConfigLate(ConfigBuilder builder) {
+        if (!ModuleEntryPoint.enabled) {
+            return;
+        }
+
         Identifier renderDistance = Identifier.of("sodium", "general.render_distance");
         builder.registerOwnModOptions()
                 .registerOptionOverlay(
