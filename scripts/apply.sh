b21fce67bb9c09b8251937a7a7aa5dd57ea9b077
ishland <30385023+ishland@users.noreply.github.com>
2026-06-16 15:54:11 +0800

    fix: remove ignore whitespace

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -61,7 +61,7 @@ function applyPatch {
     git reset --hard upstream/upstream
     echo "  Applying patches to $target..."
     git am --abort >/dev/null 2>&1
-    if ! git am --3way --ignore-whitespace "$basedir/patches/$patch_folder/"*.patch
+    if ! git am --3way "$basedir/patches/$patch_folder/"*.patch
     then
         echo "  Something did not apply cleanly to $target."
         echo "  Please review above details and finish the apply then"
