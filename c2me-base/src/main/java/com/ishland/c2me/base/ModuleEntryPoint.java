c6372e557654cff04efa4c1395ca55d689b39a7c
ishland <30385023+ishland@users.noreply.github.com>
2026-09-10 10:31:52 +0800

    new: thread priority presets
    (cherry picked from commit 168d5b553ea19763656380e2e94dd51a448e6a6d)
    (cherry picked from commit a0e9e474ec31cdc1c649d0b874c1ed53281a4747)

--- c2me-base/src/main/java/com/ishland/c2me/base/ModuleEntryPoint.java
+++ c2me-base/src/main/java/com/ishland/c2me/base/ModuleEntryPoint.java
@@ -25,13 +25,20 @@
 package com.ishland.c2me.base;
 
 import com.ishland.c2me.base.common.config.ConfigSystem;
+import com.ishland.c2me.base.common.threadpriority.ThreadPriorityPresets;
 import io.netty.util.internal.PlatformDependent;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.objecthunter.exp4j.ExpressionBuilder;
 import net.objecthunter.exp4j.function.Function;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import oshi.PlatformEnum;
+import oshi.SystemInfo;
 
 public class ModuleEntryPoint {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ModuleEntryPoint.class);
 
     private static final boolean enabled = true;
 
@@ -41,11 +48,11 @@ public class ModuleEntryPoint {
                     max(
                         1,
                         min(
-                            if( is_windows,
-                                (cpus / 1.6),
-                                (cpus / 1.3)
+                            if( is_smt_probably_enabled,
+                                cores,
+                                cores - 1
                             )  - if(is_client, 1, 0),
-                            ( ( mem_gb - (if(is_client, 1.0, 0.5)) ) / 0.6 )
+                            ( ( mem_gb - (if(is_client, 1.0, 0.5)) ) * 3.0 )
                         )
                     )
                 \040""";
@@ -56,19 +63,47 @@ public class ModuleEntryPoint {
 
                     The expression for the default value of global executor parallelism.\s
                     This is used when the parallelism isn't overridden.
-                    Available variables: is_windows, is_j9vm, is_client, cpus, mem_gb
+                    Available variables: is_windows, is_j9vm, is_client, cpus, mem_gb, cores, is_smt_probably_enabled
                     """.indent(1))
             .getString(DEFAULT_EXPRESSION, DEFAULT_EXPRESSION);
 
-    public static final long threadPoolPriority = new ConfigSystem.ConfigAccessor()
-            .key("threadPoolPriority")
+    public static final ThreadPriorityPresets threadPoolPriorityPreset = new ConfigSystem.ConfigAccessor()
+            .key("threadPoolPriorityPreset")
             .comment("""
-                    Sets the thread priority for worker threads
+                    Sets the thread priority preset for worker threads
                     
-                    References:
-                    - https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/lang/Thread.html#setPriority(int)
+                    Available presets:
+                    - UNSET: do not touch any thread priority settings, restores legacy behavior
+                    - BELOW_NORMAL:
+                      Linux: SCHED_BATCH
+                      Windows: Below Normal priority
+                      MacOS: priority=30
+                      FreeBSD: priority=12
+                    - LOW:
+                      Linux: SCHED_BATCH, nice=3
+                      Windows: Below Normal priority, enable power throttling
+                      MacOS: priority=28
+                      FreeBSD: priority=6
+                    - LOWER
+                      Linux: SCHED_BATCH, nice=8
+                      Windows: Lowest priority, enable power throttling
+                      MacOS: priority=23
+                      FreeBSD: priority=3
+                    - LOWEST
+                      Linux: SCHED_BATCH, nice=16
+                      Windows: Idle priority, enable power throttling
+                      MacOS: priority=15
+                      FreeBSD: priority=0
+                    
+                    Defaults to LOW on clients and BELOW_NORMAL for dedicated servers
+                    
+                    Please preserve quotes so the config don't break
                     """)
-            .getLong(Thread.NORM_PRIORITY - 1, Thread.NORM_PRIORITY - 1, ConfigSystem.LongChecks.POSITIVE_VALUES_ONLY);
+            .getEnum(
+                    ThreadPriorityPresets.class,
+                    FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT ? ThreadPriorityPresets.LOW : ThreadPriorityPresets.BELOW_NORMAL,
+                    ThreadPriorityPresets.LOW
+            );
 
     public static final boolean disableLoggingShutdownHook = new ConfigSystem.ConfigAccessor()
             .key("fixes.disableLoggingShutdownHook")
@@ -84,10 +119,48 @@ public class ModuleEntryPoint {
 
     public static final int defaultParallelism;
 
+    private static int fetchedCoreCount = -1;
+
+    private static int tryFetchCoreCount() {
+        if (fetchedCoreCount > 0) {
+            return fetchedCoreCount;
+        }
+        return fetchedCoreCount = tryFetchCoreCount0();
+    }
+
+    private static int tryFetchCoreCount0() {
+        int logicalProcessorsCount = Runtime.getRuntime().availableProcessors();
+        try {
+            SystemInfo systemInfo = new SystemInfo();
+            LOGGER.info("CPU name: {}", systemInfo.getHardware().getProcessor().getProcessorIdentifier().getName());
+            int coreCount = systemInfo.getHardware().getProcessor().getPhysicalProcessorCount();
+            PlatformEnum currentPlatform = SystemInfo.getCurrentPlatform();
+            switch (currentPlatform) {
+                case MACOS, LINUX, WINDOWS, ANDROID -> {
+                }
+                default -> {
+                    LOGGER.warn("The returned core count ({}) is potentially unusable on your platform {}, assuming 2/3 of the available logical cpus are physical cpus", coreCount, currentPlatform);
+                    coreCount = logicalProcessorsCount * 2 / 3;
+                }
+            }
+            if (logicalProcessorsCount < coreCount) {
+                LOGGER.warn("There's fewer logical processors than enumerated core count. Returning available logical processor count");
+                coreCount = logicalProcessorsCount;
+            }
+            return coreCount;
+        } catch (Throwable t) {
+            LOGGER.error("Failed to fetch system core count, assuming 2/3 of the available logical cpus are physical cpus");
+            return logicalProcessorsCount * 2 / 3;
+        }
+    }
+
     private static int tryEvaluateExpression(String expression) {
+        int coreCount = tryFetchCoreCount();
+        int availableProcessors = Runtime.getRuntime().availableProcessors();
+        LOGGER.info("Detected {} available physical CPUs, {} available logical CPUs", coreCount, availableProcessors);
         return (int) Math.max(1,
                 new ExpressionBuilder(expression)
-                        .variables("is_windows", "is_j9vm", "is_client", "cpus", "mem_gb")
+                        .variables("is_windows", "is_j9vm", "is_client", "cpus", "mem_gb", "cpus", "cores", "is_smt_probably_enabled")
                         .function(new Function("max", 2) {
                             @Override
                             public double apply(double... args) {
@@ -110,7 +183,9 @@ public class ModuleEntryPoint {
                         .setVariable("is_windows", PlatformDependent.isWindows() ? 1 : 0)
                         .setVariable("is_j9vm", PlatformDependent.isJ9Jvm() ? 1 : 0)
                         .setVariable("is_client", FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT ? 1 : 0)
-                        .setVariable("cpus", Runtime.getRuntime().availableProcessors())
+                        .setVariable("cpus", availableProcessors)
+                        .setVariable("cores", coreCount)
+                        .setVariable("is_smt_probably_enabled", availableProcessors > coreCount ? 1 : 0)
                         .setVariable("mem_gb", Runtime.getRuntime().maxMemory() / 1024.0 / 1024.0 / 1024.0)
                         .evaluate()
         );
