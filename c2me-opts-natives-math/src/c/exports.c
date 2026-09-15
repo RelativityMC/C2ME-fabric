38f6af4b2c3f30f82f75e5fd79edf8c4ef357ab7
ishland <30385023+ishland@users.noreply.github.com>
2026-09-15 18:19:52 +0800

    change: remove unused functions

--- c2me-opts-natives-math/src/c/exports.c
+++ c2me-opts-natives-math/src/c/exports.c
@@ -2,30 +2,6 @@
 #include <FastNoiseLite.h>
 #include <target_macros.h>
 
-TARGET_IMPL(c2me_natives_noise_perlin_sample, double, (const aligned_uint32_ptr permutations, const double originX,
-                                                       const double originY, const double originZ, const double x,
-                                                       const double y, const double z, const double yScale,
-                                                       const double yMax) {
-    return math_noise_perlin_sample(permutations, originX, originY, originZ, x, y, z, yScale, yMax);
-})
-
-TARGET_IMPL(c2me_natives_noise_perlin_double, double, (const double_octave_sampler_data_t *const data,
-                                                       const double x, const double y, const double z) {
-    return math_noise_perlin_double_octave_sample(data, x, y, z);
-})
-
-TARGET_IMPL(c2me_natives_noise_perlin_double_batch, void, (const double_octave_sampler_data_t *const data,
-                                                           double *const res, const double *const x,
-                                                           const double *const y, const double *const z,
-                                                           const uint32_t length) {
-    math_noise_perlin_double_octave_sample_batch(data, res, x, y, z, length);
-})
-
-TARGET_IMPL(c2me_natives_noise_interpolated, double, (const interpolated_noise_sampler_t *const data,
-                                                      const double x, const double y, const double z) {
-    return math_noise_perlin_interpolated_sample(data, x, y, z);
-})
-
 TARGET_IMPL(c2me_natives_end_islands_sample, float, (const aligned_uint32_ptr simplex_permutations, const int32_t x, const int32_t z) {
     return math_end_islands_sample(simplex_permutations, x, z);
 })
