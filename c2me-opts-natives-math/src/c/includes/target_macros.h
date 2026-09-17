9f813692d94da794268efbf43a71e1b29e012e2e
ishland <30385023+ishland@users.noreply.github.com>
2026-09-17 22:50:55 +0800

    change: move ice lake to gather enabled list

--- c2me-opts-natives-math/src/c/includes/target_macros.h
+++ c2me-opts-natives-math/src/c/includes/target_macros.h
@@ -8,13 +8,13 @@
 #ifdef GATHER_DISABLED
 #define TARGET_IMPL(func_prefix, func_ret, func_call) \
   __attribute__((target("arch=haswell"))) TARGET_IMPL_ARCH(avx2, func_prefix, func_ret, func_call) \
-  __attribute__((target("arch=skylake-avx512"))) TARGET_IMPL_ARCH(avx512skx, func_prefix, func_ret, func_call) \
-  __attribute__((target("arch=icelake-server"))) TARGET_IMPL_ARCH(avx512icl, func_prefix, func_ret, func_call)
+  __attribute__((target("arch=skylake-avx512"))) TARGET_IMPL_ARCH(avx512skx, func_prefix, func_ret, func_call)
 #else
 #define TARGET_IMPL(func_prefix, func_ret, func_call) \
   __attribute__((target("arch=x86-64"))) TARGET_IMPL_ARCH(sse2, func_prefix, func_ret, func_call) \
   __attribute__((target("arch=x86-64-v2"))) TARGET_IMPL_ARCH(sse4_2, func_prefix, func_ret, func_call) \
   __attribute__((target("arch=sandybridge"))) TARGET_IMPL_ARCH(avx, func_prefix, func_ret, func_call) \
+  __attribute__((target("arch=icelake-server"))) TARGET_IMPL_ARCH(avx512icl, func_prefix, func_ret, func_call) \
   __attribute__((target("arch=alderlake"))) TARGET_IMPL_ARCH(avx2vnni, func_prefix, func_ret, func_call) \
   __attribute__((target("arch=sapphirerapids"))) TARGET_IMPL_ARCH(avx512spr, func_prefix, func_ret, func_call) \
   __attribute__((target("arch=diamondrapids"))) TARGET_IMPL_ARCH(avx10_2dmr, func_prefix, func_ret, func_call) \
