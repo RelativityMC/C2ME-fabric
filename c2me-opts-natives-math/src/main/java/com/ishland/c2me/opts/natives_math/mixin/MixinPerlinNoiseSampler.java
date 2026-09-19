d0930bc9d120f79e359f826269b23ce7cca0951f
ishland <30385023+ishland@users.noreply.github.com>
2026-09-19 22:46:11 +0800

    change: also gate native entry by scale

--- c2me-opts-natives-math/src/main/java/com/ishland/c2me/opts/natives_math/mixin/MixinPerlinNoiseSampler.java
+++ c2me-opts-natives-math/src/main/java/com/ishland/c2me/opts/natives_math/mixin/MixinPerlinNoiseSampler.java
@@ -68,7 +68,7 @@ public abstract class MixinPerlinNoiseSampler extends LatticedNoiseSampler imple
             );
             return;
         }
-        if (buf.count() < 8) {
+        if (buf.count() < 8 || (region.sizeY() > 2 && scaleY < 0.2)) {
             this.fill(buf, region, scaleXz, scaleY, outputScale);
             return;
         }
